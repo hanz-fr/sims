efe6661544c3daa94b8261455ea788da649b5d63
dirarmdn <dhiraramadini@gmail.com>
2022-09-12 14:56:17 +0700

    add bootstrap 5

--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,9 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    resolve: {
+        alias: {
+            '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+        },
+    },
 });
