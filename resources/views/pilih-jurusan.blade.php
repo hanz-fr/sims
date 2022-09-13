42f28f72bd860923aae24d5916080e325f8da26c
Notnoir <riza80448@gmail.com>
2022-09-13 22:41:24 +0700

    commit di-jurusan

--- resources/views/pilih-jurusan.blade.php
+++ resources/views/pilih-jurusan.blade.php
@@ -1,76 +1,60 @@
-    @extends('layouts.main')
-    @section('content')
-    <div class="container">
-        <h1 class="tw-text-sims my-5">Buku Induk Siswa</h1>
+@extends('layouts.main')
+@section('content')
 
-        <div class="row my-5">
-            <div class="col">
-                <div class="tw-text-white tw-bg-[#1b4cff] tw-rounded-xl tw-p-5 tw-drop-shadow-md hover:tw-bg-blue-700">
-                    <div class="tw-flex">
-                        <i class="fa-solid fa-money-bills tw-text-6xl tw-text tw-py-5"></i>
-                        <p class="tw-text-lg tw-pl-5 fw-bolder">AKUTANSI DAN KEUANGAN LEMBAGA</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col">
-                <div class="tw-text-white tw-bg-[#ff2462] tw-rounded-xl tw-p-5 tw-drop-shadow-md hover:tw-bg-[#e10040]">
-                    <div class="tw-flex">
-                        <i class="fa-solid fa-cart-shopping tw-text-6xl tw-text tw-py-5"></i>
-                        <p class="tw-text-lg tw-pl-5 tw-py-7 fw-bolder">PEMASARAN</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col">
-                <div class="tw-text-white tw-bg-[#527DB9] tw-rounded-xl tw-pl-5 tw-drop-shadow-md hover:tw-bg-[#3c5d89]">
-                    <div class="tw-flex">
-                        <i class="fa-solid fa-book-bookmark tw-text-6xl tw-text tw-pt-10"></i>
-                        <p class="tw-text-lg tw-pt-7 tw-pb-3 tw-pl-5 fw-bolder">MANAJEMEN PERKANTORAN DAN LAYANAN BISNIS</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col">
-                <div class="tw-text-white tw-bg-[#FF5C5C] tw-rounded-xl tw-p-5 tw-drop-shadow-md hover:tw-bg-[#c74848]">
-                    <div class="tw-flex">
-                        <i class="fa-solid fa-box tw-text-6xl tw-text tw-py-5"></i>
-                        <p class="tw-text-lg tw-pl-5 tw-pt-5 tw-pb-2 fw-bolder">MANAJEMEN LOGISTIK</p>
-                    </div>
-                </div>
+        <h1 class="tw-text-sims my-5 tw-ml-36">Buku Induk Siswa</h1>
+    
+    <div class="tw-justify-center tw-mx-auto tw-flex tw-flex-wrap tw-gap-5">
+        <div class="tw-text-white tw-bg-[#1b4cff] tw-w-1/5 tw-rounded-xl tw-p-5 tw-drop-shadow-md hover:tw-bg-blue-700">
+            <div class="tw-flex">
+                <i class="fa-solid fa-money-bills tw-text-6xl tw-text tw-py-5"></i>
+                <p class="tw-text-lg tw-pl-5 tw-py-5 fw-bolder">AKUTANSI DAN KEUANGAN LEMBAGA</p>
             </div>
         </div>
-        <div class="row my-5">
-            <div class="col">
-                <div class="tw-text-white tw-bg-[#BF32FC] tw-rounded-xl tw-p-5 tw-drop-shadow-md hover:tw-bg-[#9727c6]">
-                    <div class="tw-flex">
-                        <i class="fa-solid fa-camera tw-text-6xl tw-text tw-py-5"></i>
-                        <p class="tw-text-lg tw-pl-5 tw-pt-5 tw-pb-2 fw-bolder">DESAIN KOMUNIKASI VISUAL</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col">
-                <div class="tw-text-white tw-bg-[#6F6D6D] tw-rounded-xl tw-pl-5 tw-drop-shadow-md hover:tw-bg-[#585656]">
-                    <div class="tw-flex">
-                        <i class="fa-solid fa-desktop tw-text-6xl tw-text tw-pt-10"></i>
-                        <p class="tw-text-lg tw-pt-7 tw-pb-3 tw-pl-5 fw-bolder">TEKNIK KOMPUTER JARINGAN DAN TELEKOMUNIKASI</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col">
-                <div class="tw-text-white tw-bg-[#2CC06F] tw-rounded-xl tw-pl-5 tw-drop-shadow-md hover:tw-bg-[#249f5b]">
-                    <div class="tw-flex">
-                        <i class="fa-solid fa-code tw-text-6xl tw-text tw-pt-10"></i>
-                        <p class="tw-text-lg tw-pt-7 tw-pb-3 tw-pl-5 fw-bolder">PENGEMBANGAN PERANGKAT LUNAK DAN GIM</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col">
-                <div class="tw-text-white tw-bg-[#4D9E9E] tw-rounded-xl tw-p-5 tw-drop-shadow-md hover:tw-bg-[#3f7f7f]">
-                    <div class="tw-flex">
-                        <i class="fa-solid fa-user-graduate tw-text-6xl tw-text tw-py-5"></i>
-                        <p class="tw-text-lg tw-pl-5 tw-pt-8 fw-bolder">ALUMNI</p>
-                    </div>
-                </div>
+        <div class="tw-text-white tw-bg-[#FF5C5C] tw-w-1/5 tw-rounded-xl tw-p-5 tw-drop-shadow-md hover:tw-bg-[#c74848]">
+            <div class="tw-flex">
+                <i class="fa-solid fa-cart-shopping tw-text-6xl tw-text tw-py-5"></i>
+                <p class="tw-text-lg tw-pl-5 tw-pt-8 fw-bolder">PEMASARAN</p>
             </div>
         </div>
+        <div class="tw-text-white tw-bg-[#527DB9] tw-w-1/5 tw-rounded-xl tw-p-5 tw-drop-shadow-md hover:tw-bg-[#3c5d89]">
+            <div class="tw-flex">
+                <i class="fa-solid fa-book-bookmark tw-text-6xl tw-text tw-py-5"></i>
+                <p class="tw-text-lg tw-pl-5 tw-pt-2 fw-bolder">MANAJEMEN PERKANTORAN DAN LAYANAN BISNIS</p>
+            </div>
+        </div>
+        <div class="tw-text-white tw-bg-[#FF5C5C] tw-w-1/5 tw-rounded-xl tw-p-5 tw-drop-shadow-md hover:tw-bg-[#c74848]">
+            <div class="tw-flex">
+                <i class="fa-solid fa-box tw-text-6xl tw-text tw-py-5"></i>
+                <p class="tw-text-lg tw-pl-5 tw-pt-8 fw-bolder">MANAJEMEN LOGISTIK</p>
+            </div>
+        </div>
+        <div class="tw-text-white tw-bg-[#BF32FC] tw-w-1/5 tw-rounded-xl tw-p-5 tw-drop-shadow-md hover:tw-bg-[#9727c6]">
+            <div class="tw-flex">
+                <i class="fa-solid fa-camera tw-text-6xl tw-text tw-py-5"></i>
+                <p class="tw-text-lg tw-pl-5 tw-py-5 fw-bolder">DESAIN KOMUNIKASI VISUAL</p>
+            </div>
+        </div>
+        <div class="tw-text-white tw-bg-[#6F6D6D] tw-w-1/5 tw-rounded-xl tw-p-5 tw-drop-shadow-md hover:tw-bg-[#585656]">
+            <div class="tw-flex">
+                <i class="fa-solid fa-desktop tw-text-6xl tw-text tw-py-5"></i>
+                <p class="tw-text-lg tw-pl-5 tw-pt-2 fw-bolder">TEKNIK KOMPUTER JARINGAN DAN TELEKOMUNIKASI</p>
+            </div>
+        </div>
+        <div class="tw-text-white tw-bg-[#2CC06F] tw-w-1/5 tw-rounded-xl tw-p-5 tw-drop-shadow-md hover:tw-bg-[#249f5b]">
+            <div class="tw-flex">
+                <i class="fa-solid fa-code tw-text-6xl tw-text tw-py-5"></i>
+                <p class="tw-text-lg tw-pl-5 tw-pt-2 fw-bolder">PENGEMBANGAN PERANGKAT LUNAK DAN GIM</p>
+            </div>
+        </div>
+        <div class="tw-text-white tw-bg-[#4D9E9E] tw-w-1/5 tw-rounded-xl tw-p-5 tw-drop-shadow-md hover:tw-bg-[#3f7f7f]">
+            <div class="tw-flex">
+                <i class="fa-solid fa-user-graduate tw-text-6xl tw-text tw-py-5"></i>
+                <p class="tw-text-lg tw-pl-5 tw-pt-8 fw-bolder">ALUMNI</p>
+            </div>
+        </div>
+    </div>
+    <div class="tw-justify-center tw-mx-auto tw-flex tw-flex-nowrap tw-gap-5">
         
     </div>
-    @endsection
+
+@endsection
