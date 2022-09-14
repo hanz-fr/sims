67c6a370f652884dbe9501962e837c674cb1a1b9
Notnoir <riza80448@gmail.com>
2022-09-14 13:34:04 +0700

    commit di-maintable

--- resources/views/di-maintable.blade.php
+++ resources/views/di-maintable.blade.php
@@ -2,85 +2,85 @@
 
 @section('content')
 
-<div class="container">
-    <h4 class="tw-font-pop tw-font-bold tw-mt-6 tw-text-sims">DATA INDUK SISWA</h4>
-    <h6 class="tw-mb-6 tw-text-slate-400">X PENGEMBANGAN PERANGKAT LUNAK DAN GIM</h6>
-    <div class="tw-flex tw-justify-between">
-        <div class="tw-flex">
-            <form action=""> 
-                <div class="relative tw-border-2 tw-rounded-lg">
-                 <input type="text" class="tw-py-1 tw-px-5 tw-border-none tw-rounded-md">
-                 <i class="fa-solid fa-magnifying-glass tw-pr-5 tw-pl-3 tw-text-slate-600"></i>
-                </div>
-            </form>
-            <div class="dropdown tw-ml-5">
-                <button class="dropdown-toggle tw-bg-gray-300 tw-py-1 tw-px-3 tw-rounded-xl tw-text" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  10
-                </button>
-                <ul class="dropdown-menu">
-                  <li><a class="dropdown-item" href="#">25</a></li>
-                  <li><a class="dropdown-item" href="#">50</a></li>
-                  <li><a class="dropdown-item" href="#">100</a></li>
-                </ul>
-            </div>     
+    <div class="container">
+        <h4 class="tw-font-pop tw-font-bold tw-mt-6 tw-text-sims">DATA INDUK SISWA</h4>
+        <h6 class="tw-mb-6 tw-text-slate-400">X PENGEMBANGAN PERANGKAT LUNAK DAN GIM</h6>
+        <div class="tw-flex tw-justify-between">
+            <div class="tw-flex">
+                <form action=""> 
+                    <div class="relative tw-border-2 tw-rounded-lg">
+                    <input type="text" class="tw-py-1 tw-px-5 tw-border-none tw-rounded-md">
+                    <i class="fa-solid fa-magnifying-glass tw-pr-5 tw-pl-3 tw-text-slate-600"></i>
+                    </div>
+                </form>
+                <div class="dropdown tw-ml-5">
+                    <button class="dropdown-toggle tw-bg-gray-300 tw-py-1 tw-px-3 tw-rounded-xl tw-text" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    10
+                    </button>
+                    <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="#">25</a></li>
+                    <li><a class="dropdown-item" href="#">50</a></li>
+                    <li><a class="dropdown-item" href="#">100</a></li>
+                    </ul>
+                </div>     
+            </div>
+            <div class="">
+                <a href=""><i class="fa-solid fa-print tw-text-xl tw-px-3 tw-py-1 tw-mb-3 tw-mx-2 tw-text-white tw-bg-sims hover:tw-bg-[#3b7a7a] tw-border tw-rounded-lg"></i></a>
+                <a href=""><i class="fa-solid fa-copy tw-text-xl tw-px-3 tw-py-1 tw-mb-3 tw-mx-2 tw-text-white tw-bg-sims hover:tw-bg-[#3b7a7a] tw-border tw-rounded-lg"></i></a>
+                <a href=""><i class="fa-solid fa-file-excel tw-text-xl tw-px-3 tw-py-1 tw-mb-3 tw-mx-2 tw-text-white tw-bg-sims hover:tw-bg-[#3b7a7a] tw-border tw-rounded-lg"></i></a>
+                <a href=""><i class="fa-solid fa-file-pdf tw-text-xl tw-px-3 tw-py-1 tw-mb-3 tw-mx-2 tw-text-white tw-bg-sims hover:tw-bg-[#3b7a7a] tw-border tw-rounded-lg"></i></a>
+            </div>
         </div>
-        <div class="">
-            <a href=""><i class="fa-solid fa-print tw-text-xl tw-px-3 tw-py-1 tw-mb-3 tw-mx-2 tw-text-white tw-bg-sims hover:tw-bg-[#3b7a7a] tw-border tw-rounded-lg"></i></a>
-            <a href=""><i class="fa-solid fa-copy tw-text-xl tw-px-3 tw-py-1 tw-mb-3 tw-mx-2 tw-text-white tw-bg-sims hover:tw-bg-[#3b7a7a] tw-border tw-rounded-lg"></i></a>
-            <a href=""><i class="fa-solid fa-file-excel tw-text-xl tw-px-3 tw-py-1 tw-mb-3 tw-mx-2 tw-text-white tw-bg-sims hover:tw-bg-[#3b7a7a] tw-border tw-rounded-lg"></i></a>
-            <a href=""><i class="fa-solid fa-file-pdf tw-text-xl tw-px-3 tw-py-1 tw-mb-3 tw-mx-2 tw-text-white tw-bg-sims hover:tw-bg-[#3b7a7a] tw-border tw-rounded-lg"></i></a>
-        </div>
-    </div>
 
-    <div class="tw-overflow-x-auto tw-relative tw-shadow-md sm:tw-rounded-xl">
-        <table class="tw-w-full tw-text-sm tw-text-center">
-            <thead class="tw-text-md tw-bg-gray-100 tw-text-basic tw-border tw-font-pop">
-                <tr>
-                    <th scope="col" class="tw-py-3 tw-px-6 tw-border-r">NO</th>
-                    <th scope="col" class="tw-py-3 tw-px-6 tw-border-r">NIS</th>
-                    <th scope="col" class="tw-py-3 tw-px-6 tw-border-r">NISN</th>
-                    <th scope="col" class="tw-py-3 tw-px-6 tw-border-r">NAMA PESERTA DIDIK</th>
-                    <th scope="col" class="tw-py-3 tw-px-6 tw-border-r">GENDER</th>
-                    <th scope="col" class="tw-py-3 tw-px-6 tw-border-r">KELAS</th>
-                    <th scope="col" class="tw-py-3 tw-px-6">ACTION</th>
-                </tr>
-            </thead>
-            <tbody class="tw-text-base">
-                <tr class="tw-bg-white tw-border">
-                    <td class="tw-py-4 tw-px-6 tw-border">01</td>
-                    <td class="tw-py-4 tw-px-6 tw-border">-</td>
-                    <td class="tw-py-4 tw-px-6 tw-border">-</td>
-                    <td class="tw-py-4 tw-px-6 tw-border">-</td>
-                    <td class="tw-py-4 tw-px-6 tw-border">-</td>
-                    <td class="tw-py-4 tw-px-6 tw-border">-</td>
-                    <td>
-                        <button class="tw-border tw-rounded-lg text-white tw-py-2 tw-px-5 tw-bg-sims hover:tw-bg-[#3b7a7a]">
-                            <i class="fa-solid fa-file-pen"></i>
-                        </button>
-                        <button class="tw-border tw-rounded-lg text-white tw-py-2 tw-px-4 tw-bg-[#949494] hover:tw-bg-[#717171]">
-                            <i class="fa-solid fa-eye"></i>
-                        </button>
-                    </td>
-                </tr>
-                <tr class="tw-bg-white tw-border">
-                    <td class="tw-py-4 tw-px-6 tw-border">01</td>
-                    <td class="tw-py-4 tw-px-6 tw-border">-</td>
-                    <td class="tw-py-4 tw-px-6 tw-border">-</td>
-                    <td class="tw-py-4 tw-px-6 tw-border">-</td>
-                    <td class="tw-py-4 tw-px-6 tw-border">-</td>
-                    <td class="tw-py-4 tw-px-6 tw-border">-</td>
-                    <td>
-                        <button class="tw-border tw-rounded-lg text-white tw-py-2 tw-px-5 tw-bg-sims hover:tw-bg-[#3b7a7a]">
-                            <i class="fa-solid fa-file-pen"></i>
-                        </button>
-                        <button class="tw-border tw-rounded-lg text-white tw-py-2 tw-px-4 tw-bg-[#949494] hover:tw-bg-[#717171]">
-                            <i class="fa-solid fa-eye"></i>
-                        </button>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
+        <div class="tw-overflow-x-auto tw-relative tw-shadow-md sm:tw-rounded-xl">
+            <table class="tw-w-full tw-text-sm tw-text-center">
+                <thead class="tw-text-md tw-bg-gray-100 tw-text-basic tw-border tw-font-pop">
+                    <tr>
+                        <th scope="col" class="tw-py-3 tw-px-6 tw-border-r">NO</th>
+                        <th scope="col" class="tw-py-3 tw-px-6 tw-border-r">NIS</th>
+                        <th scope="col" class="tw-py-3 tw-px-6 tw-border-r">NISN</th>
+                        <th scope="col" class="tw-py-3 tw-px-6 tw-border-r">NAMA PESERTA DIDIK</th>
+                        <th scope="col" class="tw-py-3 tw-px-6 tw-border-r">GENDER</th>
+                        <th scope="col" class="tw-py-3 tw-px-6 tw-border-r">KELAS</th>
+                        <th scope="col" class="tw-py-3 tw-px-6">ACTION</th>
+                    </tr>
+                </thead>
+                <tbody class="tw-text-base">
+                    <tr class="tw-bg-white tw-border">
+                        <td class="tw-py-4 tw-px-6 tw-border">01</td>
+                        <td class="tw-py-4 tw-px-6 tw-border">-</td>
+                        <td class="tw-py-4 tw-px-6 tw-border">-</td>
+                        <td class="tw-py-4 tw-px-6 tw-border">-</td>
+                        <td class="tw-py-4 tw-px-6 tw-border">-</td>
+                        <td class="tw-py-4 tw-px-6 tw-border">-</td>
+                        <td>
+                            <a href="" class="tw-border tw-rounded-lg text-white tw-py-2 tw-px-5 tw-bg-sims hover:tw-bg-[#3b7a7a]">
+                                <i class="fa-solid fa-file-pen"></i>
+                            </a>
+                            <a href="" class="tw-border tw-rounded-lg text-white tw-py-2 tw-px-4 tw-bg-[#949494] hover:tw-bg-[#717171]">
+                                <i class="fa-solid fa-eye"></i>
+                            </a>
+                        </td>
+                    </tr>
+                    <tr class="tw-bg-white tw-border">
+                        <td class="tw-py-4 tw-px-6 tw-border">01</td>
+                        <td class="tw-py-4 tw-px-6 tw-border">-</td>
+                        <td class="tw-py-4 tw-px-6 tw-border">-</td>
+                        <td class="tw-py-4 tw-px-6 tw-border">-</td>
+                        <td class="tw-py-4 tw-px-6 tw-border">-</td>
+                        <td class="tw-py-4 tw-px-6 tw-border">-</td>
+                        <td>
+                            <a href="" class="tw-border tw-rounded-lg text-white tw-py-2 tw-px-5 tw-bg-sims hover:tw-bg-[#3b7a7a]">
+                                <i class="fa-solid fa-file-pen"></i>
+                            </a>
+                            <a href="" class="tw-border tw-rounded-lg text-white tw-py-2 tw-px-4 tw-bg-[#949494] hover:tw-bg-[#717171]">
+                                <i class="fa-solid fa-eye"></i>
+                            </a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     </div>
-</div>
 
 @endsection
