a0c3b74bbeabdbacd04ea051e86dc97facfed74f
recta-rrae <raihanf787@gmail.com>
2022-09-19 13:21:50 +0700

    input siswa + ortu on

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -9,7 +9,7 @@ class ApiController extends Controller
 {
     public function index() {
 
-        $siswa = Http::get('localhost:3000/siswa');
+        $siswa = Http::get('https://ece5-114-79-49-66.ap.ngrok.io/siswa');
 
         return view('be-test',[
             'siswa' => json_decode($siswa),
@@ -25,10 +25,9 @@ class ApiController extends Controller
         ]);
     }
 
-
     public function store(Request $request) {
 
-        Http::post('localhost:3000/ortu', [
+        Http::post('https://ece5-114-79-49-66.ap.ngrok.io/ortu', [
             'nik_ortu' => null,
             'nama' => $request->nama_ibu,
             'hubungan' => 'Ibu',
@@ -38,7 +37,7 @@ class ApiController extends Controller
             'email' => $request->email_ortu,
         ]);
 
-        Http::post('localhost:3000/siswa',[
+        Http::post('https://ece5-114-79-49-66.ap.ngrok.io/siswa',[
             'nis' => $request->nis,
             'nisn' => $request->nisn,
             'nama' => $request->nama,
@@ -48,17 +47,17 @@ class ApiController extends Controller
             'nomor_ijazah_smp' => $request->nomor_ijazah_smp,
             'tanggal_ijazah_smk' => $request->tanggal_ijazah_smk,
             'nomor_skhun' => $request->nomor_skhun,
-            'tahun_ijazah_smp' => $request->tahun_ijazah_smp,
+            'tahun_ijazah_smp' => (int)$request->tahun_ijazah_smp,
             'diterima_di_kelas' => $request->kelas,
             'tgl_diterima' => $request->tgl_masuk,
-            'semester_diterima' => $request->semester,
+            'semester_diterima' => (int)$request->semester,
             'alamat_siswa' => $request->alamat_siswa,
             'nama_sekolah_asal' => $request->nama_sekolah_asal,
             'alamat_sekolah_asal' => $request->alamat_sekolah_asal,
             'tmp_lahir' => $request->tmp_lahir, 
             'tgl_lahir' => $request->tgl_lahir,
             'jenis_kelamin' => $request->jenis_kelamin,
-            'anak_ke' => $request->anak_ke,
+            'anak_ke' => (int)$request->anak_ke,
             'status' => $request->status,
             'agama' => $request->agama,
             'keterangan_lain' => $request->keterangan_lain,
@@ -72,6 +71,12 @@ class ApiController extends Controller
         ]);
 
         return $request;
+
+        /* return view('be-test2', [
+            'title' => 'Data Tidak Naik Kelas',
+            'active' => 'data-induk',
+            'req' => $request
+        ]); */
     }
 }
 
