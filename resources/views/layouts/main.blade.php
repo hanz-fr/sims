3ce23a06029e3091ad20f29392efe4c13381a395
recta-rrae <raihanf787@gmail.com>
2022-09-19 08:28:06 +0700

    perbaiki butsrep

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -10,7 +10,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,100;1,200;1,300;1,400;1,500;1,600;1,700&family=Ubuntu:wght@300;400;500;700&display=swap" rel="stylesheet">
   {{-- css --}}
   <link rel="stylesheet" href="{{ URL::asset('assets/output.css') }}">
-  <link rel="stylesheet" href="{{ URL::asset('assets/bootstrap/css/bootstrap.min.css') }}">
+  <link rel="stylesheet" href="{{ URL::asset('bootstrap/css/bootstrap.min.css') }}">
   {{-- icon --}}
   <link rel="stylesheet" href="{{ URL::asset('font-awesome/css/all.min.css') }}">
   <title>SIMS || {{ $title }}</title>
