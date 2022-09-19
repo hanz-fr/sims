aed025c962042fa7a4a195306ad2cfce04b7070e
dirarmdn <dhiraramadini@gmail.com>
2022-09-19 11:20:28 +0700

    fix again

--- resources/views/data-rekap-siswa.blade.php
+++ resources/views/data-rekap-siswa.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="tw-flex tw-flex-row tw-gap-8 tw-mt-8">
-      <div class="tw-bg-white tw-rounded-xl tw-flex tw-flex-col tw-p-14 tw-shadow-lg tw-font-pop tw-w-1/3">
+      <div class="tw-bg-white tw-rounded-xl tw-flex tw-flex-col tw-p-14 tw-shadow-lg tw-font-pop tw-w-1/4">
         <div class="tw-flex tw-flex-row tw-mx-auto tw-text-sims">
           <div class="tw-text-4xl"><i class="fa-solid fa-user-group"></i></div>
           <div class="tw-text-xl my-auto tw-ml-3 tw-font-bold">Siswa</div>
@@ -524,29 +524,29 @@
         <div class="tw-text-sims tw-text-center">
           <div class="tw-text-xl my-auto tw-ml-3 tw-font-bold tw-text-gray-500">Quick Access</div>
         </div>
-        <div class="tw-grid tw-grid-cols-2 tw-gap-8 tw-mt-8 tw-px-8">
+        <div class="tw-grid tw-grid-cols-2 tw-gap-8 tw-mt-8">
           <a href="#" class="tw-group">
-            <div class="tw-flex tw-flex-col tw-justify-center tw-text-center tw-border-2 tw-py-4 tw-px-4 tw-bg-white tw-rounded-lg group-hover:tw-text-white group-hover:tw-bg-sims tw-transition-all tw-duration-300">
-              <div class="tw-text-4xl tw-text-sims group-hover:tw-text-white"><i class="fa-solid fa-graduation-cap"></i></div>
-              <div class="tw-text-sm tw-text-gray-500 tw-font-normal tw-mt-3 group-hover:tw-text-white">Data Siswa Masuk</div>
+            <div class="tw-flex tw-flex-col tw-justify-center tw-text-center tw-border-2 tw-py-4 tw-bg-white tw-rounded-lg group-hover:tw-text-white group-hover:tw-bg-sims tw-transition-all tw-duration-300">
+              <div class="tw-text-2xl tw-text-sims group-hover:tw-text-white"><i class="fa-solid fa-graduation-cap"></i></div>
+              <div class="tw-text-xs tw-text-gray-500 tw-font-normal tw-mt-3 group-hover:tw-text-white">Data Siswa Masuk</div>
             </div>
           </a>
           <a href="#" class="tw-group">
-            <div class="tw-flex tw-flex-col tw-justify-center tw-text-center tw-border-2 tw-py-4 tw-px-4 tw-bg-white tw-rounded-lg group-hover:tw-text-white group-hover:tw-bg-sims tw-transition-all tw-duration-300">
-              <div class="tw-text-4xl tw-text-sims group-hover:tw-text-white"><i class="fa-regular fa-book-open"></i></div>
-              <div class="tw-text-sm tw-text-gray-500 tw-font-normal tw-mt-3 group-hover:tw-text-white">Data Induk Siswa</div>
+            <div class="tw-flex tw-flex-col tw-justify-center tw-text-center tw-border-2 tw-py-4 tw-bg-white tw-rounded-lg group-hover:tw-text-white group-hover:tw-bg-sims tw-transition-all tw-duration-300">
+              <div class="tw-text-2xl tw-text-sims group-hover:tw-text-white"><i class="fa-regular fa-book-open"></i></div>
+              <div class="tw-text-xs tw-text-gray-500 tw-font-normal tw-mt-3 group-hover:tw-text-white">Data Induk Siswa</div>
             </div>
           </a>
           <a href="#" class="tw-group">
-            <div class="tw-flex tw-flex-col tw-justify-center tw-text-center tw-border-2 tw-py-4 tw-px-4 tw-bg-white tw-rounded-lg group-hover:tw-text-white group-hover:tw-bg-sims tw-transition-all tw-duration-300">
-              <div class="tw-text-4xl tw-text-sims group-hover:tw-text-white"><i class="fa-solid fa-clipboard-list"></i></div>
-              <div class="tw-text-sm tw-text-gray-500 tw-font-normal tw-mt-3 group-hover:tw-text-white">Data Siswa Tidak Naik Kelas</div>
+            <div class="tw-flex tw-flex-col tw-justify-center tw-text-center tw-border-2 tw-py-4 tw-bg-white tw-rounded-lg group-hover:tw-text-white group-hover:tw-bg-sims tw-transition-all tw-duration-300">
+              <div class="tw-text-2xl tw-text-sims group-hover:tw-text-white"><i class="fa-solid fa-clipboard-list"></i></div>
+              <div class="tw-text-xs tw-text-gray-500 tw-font-normal tw-mt-3 group-hover:tw-text-white">Data Siswa Tidak Naik Kelas</div>
             </div>
           </a>
           <a href="#" class="tw-group">
-            <div class="tw-flex tw-flex-col tw-justify-center tw-text-center tw-border-2 tw-py-4 tw-px-4 tw-bg-white tw-rounded-lg group-hover:tw-text-white group-hover:tw-bg-sims tw-transition-all tw-duration-300">
-              <div class="tw-text-4xl tw-text-sims group-hover:tw-text-white"><i class="fa-solid fa-user-group"></i></div>
-              <div class="tw-text-sm tw-text-gray-500 tw-font-normal tw-mt-3 group-hover:tw-text-white">Data Perpindahan</div>
+            <div class="tw-flex tw-flex-col tw-justify-center tw-text-center tw-border-2 tw-py-4 tw-bg-white tw-rounded-lg group-hover:tw-text-white group-hover:tw-bg-sims tw-transition-all tw-duration-300">
+              <div class="tw-text-2xl tw-text-sims group-hover:tw-text-white"><i class="fa-solid fa-user-group"></i></div>
+              <div class="tw-text-xs tw-text-gray-500 tw-font-normal tw-mt-3 group-hover:tw-text-white">Data Mutasi <br><br><br></div>
             </div>
           </a>
         </div>
