1b5227cc2c4b7d0f82d4539dbea5ee21da7acc2e
Rayuzan-Mojavec <rayuzanmojavec@gmail.com>
2022-09-19 16:04:15 +0700

    utu

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,26 +1,63 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  {{-- font --}}
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    {{-- font --}}
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,100;1,200;1,300;1,400;1,500;1,600;1,700&family=Ubuntu:wght@300;400;500;700&display=swap" rel="stylesheet">
-  {{-- css --}}
-  <link rel="stylesheet" href="{{ URL::asset('assets/output.css') }}">
-  <link rel="stylesheet" href="{{ URL::asset('bootstrap/css/bootstrap.min.css') }}">
-  {{-- icon --}}
-  <link rel="stylesheet" href="{{ URL::asset('font-awesome/css/all.min.css') }}">
-  <title>SIMS || {{ $title }}</title>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,100;1,200;1,300;1,400;1,500;1,600;1,700&family=Ubuntu:wght@300;400;500;700&display=swap"
+        rel="stylesheet">
+    {{-- css --}}
+    <link rel="stylesheet" href="{{ URL::asset('assets/output.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('bootstrap/css/bootstrap.min.css') }}">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"
+        integrity="sha512-bPs7Ae6pVvhOSiIcyUClR7/q2OAsRiovw4vAkX+zJbw3ShAeeqezq50RIIcIURq7Oa20rW2n2q+fyXBNcU9lrw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css"
+        integrity="sha512-tS3S5qG0BlhnQROyJXvNjeEM4UpMXHrQfTGmbQ1gKmelCxlSEBUaxhRBj/EFTzpbP4RVSrpEikbmdJobCvhE3g=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css"
+        integrity="sha512-sMXtMNL1zRzolHYKEujM2AqCLUR9F2C4/05cdbxjjLSRvMQIciEPCQZo++nk7go3BtSuK9kfa/s+a4f4i5pLkw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    {{-- icon --}}
+    <link rel="stylesheet" href="{{ URL::asset('font-awesome/css/all.min.css') }}">
+    <title>SIMS || {{ $title }}</title>
 </head>
+
 <body class="tw-bg-back">
-  @include('components.nav-sidebar')
-  <script src="{{ URL::asset('assets/main.js') }}"></script>
-  {{-- alpine js --}}
-  <script defer src="https://unpkg.com/alpinejs@3.10.3/dist/cdn.min.js"></script>
-  {{-- chart js --}}
-  @stack('scripts')  
+    @include('components.nav-sidebar')
+    <script src="{{ URL::asset('assets/main.js') }}"></script>
+    {{-- alpine js --}}
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"
+    integrity="sha512-bPs7Ae6pVvhOSiIcyUClR7/q2OAsRiovw4vAkX+zJbw3ShAeeqezq50RIIcIURq7Oa20rW2n2q+fyXBNcU9lrw=="
+    crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    {{-- owlcarousel --}}
+    <script defer src="https://unpkg.com/alpinejs@3.10.3/dist/cdn.min.js"></script>
+    {{-- chart js --}}
+    @stack('scripts')
+    <script>
+    $('.owl-two').owlCarousel({
+      loop: false,
+      margin: 50,
+      nav: true,
+      responsive: {
+          0: {
+              items: 1
+          },
+          600: {
+              items: 3
+          },
+          1000: {
+              items: 3
+          }
+      }
+  })
+  </script>
 </body>
+
 </html>
