5803e37ed74741450e579a09f96cf892d0e97fc2
recta-rrae <raihanf787@gmail.com>
2022-09-30 18:10:39 +0700

    merge

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -13,7 +13,7 @@ class ApiController extends Controller
     /* GLOBAL VARIABLES */
     public function __construct()
     {
-        $this->api_url = 'https://c6a9-103-139-10-60.ngrok.io'; // Ganti link NGROK disini
+        $this->api_url = 'https://19b2-103-139-10-60.ngrok.io'; // Ganti link NGROK disini
 
         $this->all_siswa = Http::get("{$this->api_url}/siswa");
     }
