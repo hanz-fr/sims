755919bb956f930c84beff1b1b40fc7fdb44a3d3
dirarmdn <dhiraramadini@gmail.com>
2022-10-04 18:51:07 +0700

    lil

--- resources/views/edit-account.blade.php
+++ resources/views/edit-account.blade.php
@@ -10,25 +10,22 @@
   x-show="isModalOpen"
   x-on:click.away="isModalOpen = false"
   x-cloak
-  x-transition
->
+  x-transition>
 <div class="tw-flex tw-justify-center tw-mt-8">
   <div class="tw-max-w-2xl tw-rounded-lg tw-shadow-xl tw-bg-gray-50">
       <div class="tw-m-4">
           <label class="tw-inline-block tw-mb-2 tw-text-gray-500">File Upload</label>
           <div class="tw-flex tw-items-center tw-justify-center tw-w-full">
-              <label
-                  class="tw-flex tw-flex-col tw-w-full tw-h-32 tw-border-4 tw-border-blue-200 tw-border-dashed hover:tw-bg-gray-100 hover:tw-border-gray-300">
-                  <div class="tw-flex tw-flex-col tw-items-center tw-justify-center tw-pt-7">
-                      <svg xmlns="http://www.w3.org/2000/svg" class="tw-w-8 tw-h-8 tw-text-gray-400 group-hover:tw-text-gray-600"
-                          fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                              d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12" />
-                      </svg>
-                      <p class="tw-pt-1 tw-text-sm tw-tracking-wider tw-text-gray-400 group-hover:tw-text-gray-600">
-                          Attach a file</p>
-                  </div>
-  </div>
+              <label class="tw-flex tw-flex-col tw-w-full tw-h-32 tw-border-4 tw-border-blue-200 tw-border-dashed hover:tw-bg-gray-100 hover:tw-border-gray-300">
+              <div class="tw-flex tw-flex-col tw-items-center tw-justify-center tw-pt-7">
+                  <svg xmlns="http://www.w3.org/2000/svg" class="tw-w-8 tw-h-8 tw-text-gray-400 group-hover:tw-text-gray-600"
+                    fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12" />
+                  </svg>
+                  <p class="tw-pt-1 tw-text-sm tw-tracking-wider tw-text-gray-400 group-hover:tw-text-gray-600">Attach a file</p>
+              </div>
+          </div>
       </div>
   </div>
 </div>
@@ -49,7 +46,7 @@
         <img src="" alt="Pas Foto" srcset="" class="tw-rounded-xl tw-mb-10 tw-w-48 tw-h-52 tw-border tw-border-slate-400 tw-mx-auto md:tw-mt-20 sm:tw-mt-10">
         @else --}}
         <div class="tw-flex tw-flex-col">
-          <div class="tw-bg-admin-300 tw-p-2 tw-rounded-2xl tw-text-white tw-w-64 tw-h-56 tw-items-center tw-flex tw-justify-center"><i class="fa-solid fa-user tw-text-9xl"></i></div>
+          <div class="tw-bg-admin-300 tw-p-2 tw-rounded-2xl tw-text-white tw-w-60 tw-h-56 tw-items-center tw-flex tw-justify-center"><i class="fa-solid fa-user tw-text-9xl"></i></div>
           <div class="tw-flex tw-justify-end">
             <button x-on:click="isModalOpen = true" class="tw-bg-[#5A6C7C] -tw-mt-9 -tw-mr-4 tw-absolute tw-float-right  tw-text-white tw-py-2 tw-px-4 tw-rounded-xl"><i class="fa-solid fa-file-image tw-text-2xl"></i></button>
           </div>
@@ -102,7 +99,6 @@
         <button type="submit" class="tw-bg-[#90C2C2] hover:tw-bg-sims-400 tw-mr-8 tw-py-5 tw-px-16 tw-text-white tw-font-ubuntu tw-rounded-lg">Save Changes</button>
       </div>
     </form>
-    </div>
   </div>
 </div>
 @endsection
