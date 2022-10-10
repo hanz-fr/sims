65be1679be06762b13070ace89ec817ae410c229
recta-rrae <raihanf787@gmail.com>
2022-10-10 14:27:25 +0700

    added 'auth' and 'admin' middleware

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -15,7 +15,7 @@ class ApiController extends Controller
     /* GLOBAL VARIABLES */
     public function __construct()
     {
-        $this->api_url = 'https://d445-114-79-49-236.ap.ngrok.io'; // Ganti link NGROK disini
+        $this->api_url = 'https://f474-103-139-10-131.ngrok.io'; // Ganti link NGROK disini
     }
 
     /* API SISWA */
--- routes/web.php
+++ routes/web.php
@@ -18,233 +18,272 @@ use App\Http\Controllers\UserController;
 */
 
 
-/* ROUTE SISWA */
+Route::middleware(['auth'])->group(function () {
 
-Route::get('/data-induk-siswa', [ApiController::class, 'getAllSiswa']);
-Route::get('/detail/{nis}', [ApiController::class, 'getSiswa']);
-Route::get('/tambah-data', [ApiController::class, 'createSiswa']);
-Route::get('/edit-siswa/{nis}', [ApiController::class, 'editSiswa']);
+    /* MAIN DASHBOARD */
 
-Route::post('/api/siswa', [ApiController::class, 'storeSiswa']);
-Route::put('/api/siswa/update/{nis}', [ApiController::class, 'updateSiswa']);
-Route::delete('/api/siswa/delete/{nis}', [ApiController::class, 'deleteSiswa']);
+    Route::get('/', function () {
+        return view('dashboard-main', [
+            'title' => 'Dashboard',
+            'active' => 'dashboard-main'
+        ]);
+    });
 
 
+    /* ROUTE SISWA */
 
-/* ROUTE MUTASI */
+    Route::get('/data-induk-siswa', [ApiController::class, 'getAllSiswa']);
+    Route::get('/detail/{nis}', [ApiController::class, 'getSiswa']);
+    Route::get('/tambah-data', [ApiController::class, 'createSiswa']);
+    Route::get('/edit-siswa/{nis}', [ApiController::class, 'editSiswa']);
 
-// siswa-keluar
-Route::get('/siswa-keluar', [ApiController::class, 'getAllMutasiKeluar']);
-Route::get('/create-mutasi-keluar', [ApiController::class, 'createMutasiKeluar']);
-Route::get('/edit-mutasi-keluar/{id}', [ApiController::class, 'editMutasiKeluar']);
-
-Route::post('/api/mutasi-keluar/store', [ApiController::class, 'storeMutasiKeluar']);
-Route::put('/api/mutasi-keluar/update/{id}', [ApiController::class, 'updateMutasiKeluar']);
-Route::delete('/api/mutasi-keluar/delete/{id}', [ApiController::class, 'deleteMutasiKeluar']);
-
-// siswa-masuk
-Route::get('/siswa-masuk', [ApiController::class, 'getAllMutasiMasuk']);
-Route::get('/create-mutasi-masuk', [ApiController::class, 'createMutasiMasuk']);
-Route::get('/edit-mutasi-masuk/{id}', [ApiController::class, 'editMutasiMasuk']);
-
-Route::post('/api/mutasi-masuk/store', [ApiController::class, 'storeMutasiMasuk']);
-Route::put('/api/mutasi-masuk/update/{id}', [ApiController::class, 'updateMutasiMasuk']);
-Route::delete('/api/mutasi-masuk/delete/{id}', [ApiController::class, 'deleteMutasiMasuk']);
+    Route::post('/api/siswa', [ApiController::class, 'storeSiswa']);
+    Route::put('/api/siswa/update/{nis}', [ApiController::class, 'updateSiswa']);
+    Route::delete('/api/siswa/delete/{nis}', [ApiController::class, 'deleteSiswa']);
 
 
 
-/* ROUTE KELAS */
+    /* ROUTE MUTASI */
 
-Route::get('/all-kelas', function () {
-    return view('all-kelas', [
-        'title' => 'List Semua Kelas',
-        'active' => 'data-induk'
-    ]);
+    // siswa-keluar
+    Route::get('/siswa-keluar', [ApiController::class, 'getAllMutasiKeluar']);
+    Route::get('/create-mutasi-keluar', [ApiController::class, 'createMutasiKeluar']);
+    Route::get('/edit-mutasi-keluar/{id}', [ApiController::class, 'editMutasiKeluar']);
+
+    Route::post('/api/mutasi-keluar/store', [ApiController::class, 'storeMutasiKeluar']);
+    Route::put('/api/mutasi-keluar/update/{id}', [ApiController::class, 'updateMutasiKeluar']);
+    Route::delete('/api/mutasi-keluar/delete/{id}', [ApiController::class, 'deleteMutasiKeluar']);
+
+    // siswa-masuk
+    Route::get('/siswa-masuk', [ApiController::class, 'getAllMutasiMasuk']);
+    Route::get('/create-mutasi-masuk', [ApiController::class, 'createMutasiMasuk']);
+    Route::get('/edit-mutasi-masuk/{id}', [ApiController::class, 'editMutasiMasuk']);
+
+    Route::post('/api/mutasi-masuk/store', [ApiController::class, 'storeMutasiMasuk']);
+    Route::put('/api/mutasi-masuk/update/{id}', [ApiController::class, 'updateMutasiMasuk']);
+    Route::delete('/api/mutasi-masuk/delete/{id}', [ApiController::class, 'deleteMutasiMasuk']);
+    
+
+    /* GET DATA INDUK BY JURUSAN */
+
+    Route::get('/jurusan', function () {
+        return view('induk.select-jurusan', [
+            'title' => 'Pilih Jurusan',
+            'active' => 'data-induk'
+        ]);
+    });
+
+
+
+    /* ANGKATAN */
+
+    Route::get('/angkatan', function () {
+        return view('induk.select-angkatan', [
+            'title' => 'Pilih Angkatan',
+            'active' => 'data-induk'
+        ]);
+    });
+
+
+    /* ALUMNI */
+
+    Route::get('/data-alumni', function () {
+        return view('induk.show-alumni', [
+            'title' => 'Data Alumni',
+            'active' => 'data-induk'
+        ]);
+    });
+    
+
+    /* REKAP NILAI */
+
+    Route::get('/rekap-nilai', function () {
+        return view('induk.show-rekap-nilai', [
+            'title' => 'Rekap Nilai',
+            'active' => 'data-induk'
+        ]);
+    });
+    
+
+    /* REKAP SISWA */
+
+    Route::get('/rekap-siswa', function () {
+        return view('data-rekap-siswa', [
+            'title' => 'Data Rekap Siswa',
+            'active' => 'rekap-siswa'
+        ]);
+    });
+    
+
+    /* REKAP JUMLAH SISWA */
+
+    Route::get('/rekap-jumlah-siswa', function () {
+        return view('data-rekap-jumlah-siswa', [
+            'title' => 'Data Rekap Siswa',
+            'active' => 'rekap-siswa'
+        ]);
+    });
+
+
+    /* DATA TIDAK NAIK KELAS */
+
+    Route::get('/data-tidak-naik', function () {
+        return view('data-tidak-naik', [
+            'title' => 'Data Tidak Naik Kelas',
+            'active' => 'data-induk'
+        ]);
+    });
+    
+
+    /* PROFILE */
+    Route::get('/profile', [UserController::class, 'show'])->name('profile');
+    Route::get('/edit-profile', [UserController::class, 'edit']);
+    Route::post('/update-profile/{id}', [UserController::class, 'update']);
+    Route::post('/change-password', [UserController::class, 'changePassword']);
 });
 
-Route::get('/detail-kelas', function () {
-    return view('detail-kelas', [
-        'title' => 'Detail Kelas',
-        'active' => 'data-induk'
-    ]);
-});
 
-Route::get('/', function () {
-    return view('dashboard-main', [
-        'title' => 'Dashboard',
-        'active' => 'dashboard-main'
-    ]);
-});
 
-Route::get('/jurusan', function () {
-    return view('induk.select-jurusan', [
-        'title' => 'Pilih Jurusan',
-        'active' => 'data-induk'
-    ]);
-});
-
-Route::get('/angkatan', function () {
-    return view('induk.select-angkatan', [
-        'title' => 'Pilih Angkatan',
-        'active' => 'data-induk'
-    ]);
-});
-
-Route::get('/data-alumni', function () {
-    return view('induk.show-alumni', [
-        'title' => 'Data Alumni',
-        'active' => 'data-induk'
-    ]);
-});
-
-Route::get('/rekap-nilai', function () {
-    return view('induk.show-rekap-nilai', [
-        'title' => 'Rekap Nilai',
-        'active' => 'data-induk'
-    ]);
-});
-
-/* Route::get('/siswa-masuk', function () {
-    return view('siswa-masuk', [
-        'title' => 'Data Siswa Masuk',
-        'active' => 'data-induk'
-    ]);
-}); */
-
-Route::get('/rekap-siswa', function () {
-    return view('data-rekap-siswa', [
-        'title' => 'Data Rekap Siswa',
-        'active' => 'rekap-siswa'
-    ]);
-});
-
-Route::get('/rekap-jumlah-siswa', function () {
-    return view('data-rekap-jumlah-siswa', [
-        'title' => 'Data Rekap Siswa',
-        'active' => 'rekap-siswa'
-    ]);
-});
- 
-Route::get('/data-tidak-naik', function () {
-    return view('data-tidak-naik', [
-        'title' => 'Data Tidak Naik Kelas',
-        'active' => 'data-induk'
-    ]);
-});
-
-Route::get('/profile', function () {
-    return view('profil-user', [
-        'title' => 'Profile'
-    ]);
-});
-
-Route::get('/profile', [UserController::class, 'show']);
-
-Route::get('/testurl', function() {
-    return view('plswork', [
-        'title' => 'Data Tidak Naik Kelas',
-        'active' => 'data-induk'
-    ]);
-});
 
 /* ROUTE ADMIN */
-Route::get('/admin', function () {
-    return view('login-admin', [
-        'title' => 'Admin Login',
-        'active' => 'admin'
-    ]);
+
+Route::middleware(['admin'])->group(function() {
+
+    /* SHOW ALL KELAS */
+    Route::get('/all-kelas', function () {
+        return view('all-kelas', [
+            'title' => 'List Semua Kelas',
+            'active' => 'data-induk'
+        ]);
+    });
+    
+    /* SHOW KELAS DETAIL */
+    Route::get('/detail-kelas', function () {
+        return view('detail-kelas', [
+            'title' => 'Detail Kelas',
+            'active' => 'data-induk'
+        ]);
+    });
+    
+    /* ADMIN LOGIN */
+    Route::get('/admin', function () {
+        return view('login-admin', [
+            'title' => 'Admin Login',
+            'active' => 'admin'
+        ]);
+    });
+    
+    
+    /* ADMIN DASHBOARD */
+    Route::get('/dashboard', function () {
+        return view('dashboard-admin', [
+            'title' => 'Dashboard Admin',
+            'active' => 'admin'
+        ]);
+    });
+    
+    
+    /* MANAGE USER */
+    Route::get('/manage-user', function () {
+        return view('manage-user', [
+            'title' => 'Manage User SIMS',
+            'active' => 'account'
+        ]);
+    });
+    
+    
+    /* MAPEL JURUSAN */
+    Route::get('/mata-pelajaran-jurusan', function () {
+        return view('mapel-jurusan', [
+            'title' => 'Mata Pelajaran',
+            'active' => 'database'
+        ]);
+    });
+    
+    
+    /* DETAIL MAPEL JURUSAN */
+    Route::get('/detail-mata-pelajaran-jurusan', function () {
+        return view('detail-mapel-jurusan', [
+            'title' => 'Mata Pelajaran',
+            'active' => 'database'
+        ]);
+    });
+    
+    
+    /* CREATE MAPEL JURUSAN */
+    Route::get('/create-mata-pelajaran-jurusan', function () {
+        return view('create-mapel-jurusan', [
+            'title' => 'Mata Pelajaran',
+            'active' => 'database'
+        ]);
+    });
+    
+    
+    /* SHOW USER DETAIL */
+    Route::get('/show-detail', function () {
+        return view('account-detail', [
+            'title' => 'Account Details',
+            'active' => 'account'
+        ]);
+    });
+    
+    
+    /* EDIT USER */
+    Route::get('/edit-account', function () {
+        return view('edit-account', [
+            'title' => 'Edit Account',
+            'active' => 'account'
+        ]);
+    });
+    
+    
+    /* CREATE USER */
+    Route::get('/create-account', function () {
+        return view('create-account', [
+            'title' => 'Create Account',
+            'active' => 'account'
+        ]);
+    });
+    
+    
+    /* SHOW ALL JURUSAN */
+    Route::get('/show-jurusan', function () {
+        return view('show-jurusan', [
+            'title' => 'Jurusan',
+            'active' => 'database'
+        ]);
+    });
+    
+    
+    /* SHOW JURUSAN DETAIL */
+    Route::get('/show-detail-jurusan', function () {
+        return view('show-detail-jurusan', [
+            'title' => 'Detail Jurusan',
+            'active' => 'database'
+        ]);
+    });
+    
+    
+    /* CREATE JURUSAN */
+    Route::get('/create-show-jurusan', function () {
+        return view('create-show-jurusan', [
+            'title' => 'Create Jurusan',
+            'active' => 'database'
+        ]);
+    });
+    
+    
+    /* LIVE SEARCH TEST */
+    Route::get('/live-search-test', [ApiController::class, 'indexLiveSearch']);
+    
+    Route::get('/search', [ApiController::class, 'search']);
+
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard-admin', [
-        'title' => 'Dashboard Admin',
-        'active' => 'admin'
-    ]);
-});
 
-Route::get('/manage-user', function () {
-    return view('manage-user', [
-        'title' => 'Manage User SIMS',
-        'active' => 'account'
-    ]);
-});
-
-Route::get('/mata-pelajaran-jurusan', function () {
-    return view('mapel-jurusan', [
-        'title' => 'Mata Pelajaran',
-        'active' => 'database'
-    ]);
-});
-
-Route::get('/detail-mata-pelajaran-jurusan', function () {
-    return view('detail-mapel-jurusan', [
-        'title' => 'Mata Pelajaran',
-        'active' => 'database'
-    ]);
-});
-
-Route::get('/create-mata-pelajaran-jurusan', function () {
-    return view('create-mapel-jurusan', [
-        'title' => 'Mata Pelajaran',
-        'active' => 'database'
-    ]);
-});
-
-Route::get('/show-detail', function () {
-    return view('account-detail', [
-        'title' => 'Account Details',
-        'active' => 'account'
-    ]);
-});
-
-Route::get('/edit-account', function () {
-    return view('edit-account', [
-        'title' => 'Edit Account',
-        'active' => 'account'
-    ]);
-});
-
-Route::get('/create-account', function () {
-    return view('create-account', [
-        'title' => 'Create Account',
-        'active' => 'account'
-    ]);
-});
-
-Route::get('/show-jurusan', function () {
-    return view('show-jurusan', [
-        'title' => 'Jurusan',
-        'active' => 'database'
-    ]);
-});
-
-Route::get('/show-detail-jurusan', function () {
-    return view('show-detail-jurusan', [
-        'title' => 'Detail Jurusan',
-        'active' => 'database'
-    ]);
-});
-
-Route::get('/create-show-jurusan', function () {
-    return view('create-show-jurusan', [
-        'title' => 'Create Jurusan',
-        'active' => 'database'
-    ]);
-});
-
-Route::get('/live-search-test', [ApiController::class, 'indexLiveSearch']);
-
-Route::get('/search', [ApiController::class, 'search']);
-
-// ROUTE USER
+// ROUTE USER SIMS (LOGIN & REGISTER)
 Route::get('/register', [UserController::class, 'registration']);
 Route::get('/login', [UserController::class, 'index'])->name('login');
 Route::post('/registeruser', [UserController::class, 'register']);
 Route::post('/loginuser', [UserController::class, 'authenticate']);
 Route::get('/signout', [UserController::class, 'signOut']);
-
-// profile
-Route::get('/profile', [UserController::class, 'show'])->name('profile');
-Route::get('/edit-profile', [UserController::class, 'edit']);
-Route::post('/update-profile/{id}', [UserController::class, 'update']);
-Route::post('/change-password', [UserController::class, 'changePassword']);
