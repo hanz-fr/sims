da2ea0757bf3e68ebca924c5dec6660653afa6aa
dirarmdn <dhiraramadini@gmail.com>
2022-11-09 08:39:35 +0700

    test

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -21,7 +21,7 @@ class ApiController extends Controller
     /* GLOBAL VARIABLES */
     public function __construct()
     {
-        $this->api_url = '127.0.0.1:3000'; // Ganti link NGROK disini
+        $this->api_url = 'https://4630-103-148-113-86.ap.ngrok.io'; // Ganti link NGROK disini
 
 
         $this->sims_url = 'http://127.0.0.1:8000'; // SIMS URL
