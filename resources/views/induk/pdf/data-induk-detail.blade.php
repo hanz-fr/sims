a6a7dd5481422d102c2977fb9242407aa64069a3
dirarmdn <dhiraramadini@gmail.com>
2022-11-16 08:22:06 +0700

    edit remember_me

--- resources/views/induk/pdf/data-induk-detail.blade.php
+++ resources/views/induk/pdf/data-induk-detail.blade.php
@@ -6,18 +6,12 @@
       font-family: Arial, sans-serif;
     }
 
-  #data {
+  table {
     border-collapse: collapse;
-    width: 100%;
+    widtd: 100%;
     font-size: 12px
   }
 
-  #data td, #data th {
-    border: 1px solid black;
-    padding: 10px 7px;
-    color: black;
-    text-align: center;
-  }
 
   .foto {
     border: 1px solid black;
@@ -26,7 +20,7 @@
     display: flex;
     flex-direction: column;
     justify-content: space-between;
-    width: 141px;
+    widtd: 141px;
     height: 200px;
   }
 
@@ -48,7 +42,7 @@
 		justify-items: center;
   }
 
-	th {
+	td {
 		text-align: left;
 	}
 
@@ -59,7 +53,7 @@
 	<table style="width: 100%;">
 		<tbody>
 			<tr>
-				<td class="foto-foto">
+				<td>
 					<div class="pas-foto">
 						{{-- <h3 style="text-align: center;">SMKN 11 <br> BANDUNG</h3> --}}
 						<div class="foto">
@@ -69,7 +63,7 @@
 						</div>
 					</div>
 				</td>
-				<td class="foto-foto">
+				<td>
 					<div class="pas-foto">
 						{{-- <h3 style="text-align: center;">SMKN 11 <br> BANDUNG</h3> --}}
 						<div class="foto">
@@ -89,19 +83,23 @@
 <table>
 	<tbody style="text-align: left;">
 		<tr>
-			<th>NIS / NISN</th>
+			<th>1</th>
+			<td>NIS / NISN</td>
 			<td>: {{ $siswa->nis_siswa }} / {{ $siswa->nisn_siswa }}</td>
 		</tr>
 		<tr>
-			<th>Nama Peserta Didik</th>
+			<th>1</th>
+			<td>Nama Peserta Didik</td>
 			<td>: {{ $siswa->nama_siswa }}</td>
 		</tr>
 		<tr>
-			<th>Tempat Tgl. Lahir</th>
+			<th>1</th>
+			<td>Tempat Tgl. Lahir</td>
 			<td>: {{ $siswa->tmp_lahir }}, {{ $tgl_lahir_siswa }}</td>
 		</tr>
 		<tr>
-			<th>Jenis Kelamin</th>
+			<th>1</th>
+			<td>Jenis Kelamin</td>
 			<td>:                                                         
 				@if ($siswa->jenis_kelamin == 'L')
 				Laki-laki
@@ -112,16 +110,20 @@
 			   @endif</td>
 		</tr>
 		<tr>
-			<th>Agama</th>
+			<th>1</th>
+			<td>Agama</td>
 			<td>: {{ $siswa->agama }}</td>
 		</tr>
 		<tr>
-			<th>Anak Ke</th>
+			<th>1</th>
+			<td>Anak Ke</td>
 			<td>: {{ $siswa->anak_ke }}</td>
 		</tr>
 		<tr>
-			<th>Status dalam Keluarga</th>
-			<td>:                                                  @if ($siswa->status == 'AK')
+			<th>1</th>
+			<td>Status dalam Keluarga</td>
+			<td>:                                                  
+			@if ($siswa->status == 'AK')
 				Anak Kandung
 			@elseif($siswa->status == 'AT')
 				Anak Tiri
@@ -130,151 +132,184 @@
 			@endif</td>
 		</tr>
 		<tr>
-			<th>Alamat Peserta Didik</th>
+			<th>1</th>
+			<td>Alamat Peserta Didik</td>
 			<td>: {{ $siswa->alamat_siswa }}</td>
 		</tr>
 		<tr>
-			<th></th>
+			<td></td>
 			<td>No.Telp/HP : {{ $siswa->no_telp_siswa }}</td>
 		</tr>
 		<tr>
-			<th>Alamat e-mail</th>
+			<th>2</th>
+			<td>Alamat e-mail</td>
 			<td>: {{ $siswa->email_siswa }}</td>
 		</tr>
 		<tr>
-			<th>Diterima di sekolah ini</th>
+			<th>3</th>
+			<td>Diterima di sekolah ini</td>
 			<td>: </td>
 		</tr>
 		<tr>
-			<th>Di kelas</th>
+			<th>4</th>
+			<td>Di kelas</td>
 			<td>: {{ $siswa->diterima_di_kelas }}</td>
 		</tr>
 		<tr>
-			<th>Pada Tanggal</th>
+			<th>5</th>
+			<td>Pada Tanggal</td>
 			<td>: {{ $siswa->tgl_diterima }}</td>
 		</tr>
 		<tr>
-			<th>Sekolah Asal</th>
+			<th>1</th>
+			<td>Sekolah Asal</td>
 			<td>: </td>
 		</tr>
 		<tr>
-			<th>Nama Sekolah</th>
+			<th>1</th>
+			<td>Nama Sekolah</td>
 			<td>: {{ $siswa->sekolah_asal }}</td>
 		</tr>
 		<tr>
-			<th>Alamat</th>
+			<th>1</th>
+			<td>Alamat</td>
 			<td>: {{ $siswa->alamat_sekolah_asal }}</td>
 		</tr>
 		<tr>
-			<th>Ijazah SMP/MTs</th>
+			<th>1</th>
+			<td>Ijazah SMP/MTs</td>
 			<td>: </td>
 		</tr>
 		<tr>
-			<th>Tahun</th>
+			<th>1</th>
+			<td>Tahun</td>
 			<td>: {{ $siswa->thn_ijazah_smp }}</td>
 		</tr>
 		<tr>
-			<th>Nomor</th>
+			<th>1</th>
+			<td>Nomor</td>
 			<td>: {{ $siswa->no_ijazah_smp }}</td>
 		</tr>
 		<tr>
-			<th>SKHUN SMP/MTs</th>
+			<th>1</th>
+			<td>SKHUN SMP/MTs</td>
 			<td>: </td>
 		</tr>
 		<tr>
-			<th>Tahun</th>
+			<th>1</th>
+			<td>Tahun</td>
 			<td>: {{ $siswa->thn_ijazah_smp }}</td>
 		</tr>
 		<tr>
-			<th>Nomor</th>
+			<th>1</th>
+			<td>Nomor</td>
 			<td>: {{ $siswa->no_skhun_smp }}</td>
 		</tr>
 		<tr>
-			<th>Nama Orang Tua</th>
+			<th>1</th>
+			<td>Nama Orang Tua</td>
 			<td>: </td>
 		</tr>
 		<tr>
-			<th>a. Ayah</th>
+			<th>1</th>
+			<td>a. Ayah</td>
 			<td>: {{ $siswa->nama_ayah }}</td>
 		</tr>
 		<tr>
-			<th>b. Ibu</th>
+			<th>1</th>
+			<td>b. Ibu</td>
 			<td>: {{ $siswa->nama_ibu }}</td>
 		</tr>
 		<tr>
-			<th>Alamat Orang Tua</th>
+			<th>1</th>
+			<td>Alamat Orang Tua</td>
 			<td>: {{ $siswa->alamat_ortu }}</td>
 		</tr>
 		<tr>
-			<th></th>
+			<td></td>
 			<td>No. Telp/HP : {{ $siswa->no_telp_ortu }}</td>
 		</tr>
 		<tr>
-			<th></th>
+			<td></td>
 			<td>: {{ $siswa->email_ortu }}</td>
 		</tr>
 		<tr>
-			<th>Nama Wali</th>
+			<th>1</th>
+			<td>Nama Wali</td>
 			<td>: {{ $siswa->nama_wali }}</td>
 		</tr>
 		<tr>
-			<th>Alamat</th>
+			<th>1</th>
+			<td>Alamat</td>
 			<td>: {{ $siswa->alamat_wali }}</td>
 		</tr>
 		<tr>
-			<th></th>
+			<td></td>
 			<td>:No. Telp/HP {{ $siswa->no_telp_wali }}</td>
 		</tr>
 		<tr>
-			<th>Pekerjaan Wali</th>
+			<th>1</th>
+			<td>Pekerjaan Wali</td>
 			<td>: {{ $siswa->pekerjaan_wali }}</td>
 		</tr>
 		<tr>
-			<th>Meninggalkan Sekolah</th>
+			<th>1</th>
+			<td>Meninggalkan Sekolah</td>
 			<td>: </td>
 		</tr>
 		<tr>
-			<th>Tanggal</th>
+			<th>1</th>
+			<td>Tanggal</td>
 			<td>: {{ $siswa->tgl_meninggalkan_sekolah }}</td>
 		</tr>
 		<tr>
-			<th>Alasan</th>
+			<th>1</th>
+			<td>Alasan</td>
 			<td>: {{ $siswa->alasan_meninggalkan_sekolah }}</td>
 		</tr>
 		<tr>
-			<th>Tamat Di sekolah ini</th>
+			<th>1</th>
+			<td>Tamat Di sekolah ini</td>
 			<td>: </td>
 		</tr>
 		<tr>
-			<th>a. Nomor Ijazah</th>
+			<th>1</th>
+			<td>a. Nomor Ijazah</td>
 			<td>: {{ $siswa->no_ijazah_smk }}</td>
 		</tr>
 		<tr>
-			<th>a. Tanggal Ijazah</th>
+			<th>1</th>
+			<td>a. Tanggal Ijazah</td>
 			<td>: {{ $siswa->tgl_ijazah_smk }}</td>
 		</tr>
 		<tr>
-			<th>Keterangan lain-lain</th>
+			<th>1</th>
+			<td>Keterangan lain-lain</td>
 			<td>: {{ $siswa->keterangan_lain }}</td>
 		</tr>
 		<tr>
-			<th>Keterangan Jasmani</th>
+			<td></td>
+			<td>Keterangan Jasmani</td>
 			<td>: </td>
 		</tr>
 		<tr>
-			<th>Berat Badan</th>
+			<th>1</th>
+			<td>Berat Badan</td>
 			<td>: {{ $siswa->berat_badan }}</td>
 		</tr>
 		<tr>
-			<th>Tinggi Badan</th>
+			<th>1</th>
+			<td>Tinggi Badan</td>
 			<td>: {{ $siswa->tinggi_badan }}</td>
 		</tr>
 		<tr>
-			<th>Lingkar Kepala</th>
+			<th>1</th>
+			<td>Lingkar Kepala</td>
 			<td>: {{ $siswa->lingkar_kepala }}</td>
 		</tr>
 		<tr>
-			<th>Golongan Darah</th>
+			<th>1</th>
+			<td>Golongan Darah</td>
 			<td>: {{ $siswa->golongan_darah }}</td>
 		</tr>
 	</tbody>
