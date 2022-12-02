1892f695886c32b848ab698752f0250dd0fa27a4
dirarmdn <dhiraramadini@gmail.com>
2022-12-02 21:17:42 +0700

    change register to the old one (temporary)

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,7 +19,6 @@ class User extends Authenticatable
     protected $fillable = [
         'nip',
         'nama',
-        'phone',
         'email',
         'role',
         'password',
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -22,198 +22,94 @@
 
     <div class="tw-mx-auto tw-my-14 tw-container">
         <!-- card -->
-        <div id="phone_input" class="tw-bg-white tw-flex lg:tw-flex-row sm:tw-flex-col-reverse tw-mx-auto tw-px-20 tw-py-12 tw-w-2/3 tw-h-3/5 tw-border tw-border-slate-200 tw-shadow-xl tw-p-5">
+        <div class="tw-bg-white tw-flex lg:tw-flex-row sm:tw-flex-col-reverse tw-mx-auto tw-px-20 tw-py-12 tw-w-2/3 tw-h-3/5 tw-border tw-border-slate-200 tw-shadow-xl tw-p-5">
             <!-- form section -->
             <section class="lg:tw-w-3/5 sm:tw-w-full tw-font-pop">
                 <div class="tw-text-3xl tw-text-sims-400 tw-font-bold md:tw-mt-3 sm:tw-text-center lg:tw-text-left">Registrasi</div>
                 <div class="tw-text-sm tw-mt-2 tw-text-slate-400 sm:tw-text-center lg:tw-text-left">Daftar untuk membuat akun baru</div>
 
-                <form action="/registeruser" method="post" class="tw-mt-10 lg:tw-pr-8">
+            <form action="/registeruser" method="post" class="tw-mt-10 lg:tw-pr-8">
                 @csrf
-                <div style="display: block;">
-                    <ul class="tw-flex tw-flex-col tw-gap-5">
-                        <li>
-                            <input type="text" name="nama" @error('nama') is-invalid @enderror placeholder="Nama" class="login-input">
-                            @error('nama')
-                                <div class="tw-text-sm tw-text-pink-700 tw-mt-1 tw-font-ubuntu">{{ $message }}</div>
-                            @enderror
-                        </li>
-                        <li>
-                            <input type="text" name="nip" @error('nip') is-invalid @enderror placeholder="NIP" minlength="9" maxlength="18" class="login-input" required>
-                            @error('nip')
-                                <div class="tw-text-sm tw-text-pink-700 tw-mt-1 tw-font-ubuntu">{{ $message }}</div>
-                            @enderror
-                        </li>
-                        <li>
-                            <input type="text" name="phone" id="number" @error('phone') is-invalid @enderror placeholder="Nomor Telepon (+62)" maxlength="20" class="login-input" required>
-                            @error('phone')
-                                <div class="tw-text-sm tw-text-pink-700 tw-mt-1 tw-font-ubuntu">{{ $message }}</div>
-                            @enderror
-                        </li>
-                        <li>
-                            <input type="email" name="email" @error('email') is-invalid @enderror placeholder="Email" class="login-input">
-                            @error('email')
-                                <div class="tw-text-sm tw-text-pink-700 tw-mt-1 tw-font-ubuntu">{{ $message }}</div>
-                            @enderror
-                        </li>
-                        <li x-data="{ show: true }" class="tw-flex">
-                            <div class="tw-relative tw-w-full">
-                            <input name="password" id="password" @error('password') is-invalid @enderror placeholder="Kata Sandi" :type="show ? 'password' : 'text'" class="login-input" required>
-                            <div class="tw-absolute tw-inset-y-0 tw-right-0 tw-pr-3 tw-flex tw-items-center tw-text-sm tw-leading-5">
-    
-                                <svg class="tw-h-5 tw-text-gray-500" fill="none" @click="show = !show"
-                                :class="{'tw-hidden': !show, 'tw-block':show }" xmlns="http://www.w3.org/2000/svg"
-                                viewbox="0 0 576 512">
-                                <path fill="currentColor"
-                                    d="M572.52 241.4C518.29 135.59 410.93 64 288 64S57.68 135.64 3.48 241.41a32.35 
-                                    32.35 0 0 0 0 29.19C57.71 376.41 165.07 448 288 448s230.32-71.64 284.52-177.41a32.35 
-                                    32.35 0 0 0 0-29.19zM288 400a144 144 0 1 1 144-144 143.93 143.93 0 0 1-144 144zm0-240a95.31 
-                                    95.31 0 0 0-25.31 3.79 47.85 47.85 0 0 1-66.9 66.9A95.78 95.78 0 1 0 288 160z">
-                                </path>
-                                </svg>
-    
-                                <svg class="tw-h-5 tw-text-gray-500" fill="none" @click="show = !show"
-                                :class="{'tw-block': !show, 'tw-hidden':show }" xmlns="http://www.w3.org/2000/svg"
-                                viewbox="0 0 640 512">
-                                <path fill="currentColor"
-                                    d="M320 400c-75.85 0-137.25-58.71-142.9-133.11L72.2 185.82c-13.79 17.3-26.48 
-                                    35.59-36.72 55.59a32.35 32.35 0 0 0 0 29.19C89.71 376.41 197.07 448 320 448c26.91 
-                                    0 52.87-4 77.89-10.46L346 397.39a144.13 144.13 0 0 1-26 2.61zm313.82 58.1l-110.55-85.44a331.25 
-                                    331.25 0 0 0 81.25-102.07 32.35 32.35 0 0 0 0-29.19C550.29 135.59 442.93 64 320 64a308.15 308.15 
-                                    0 0 0-147.32 37.7L45.46 3.37A16 16 0 0 0 23 6.18L3.37 31.45A16 16 0 0 0 6.18 53.9l588.36 454.73a16 
-                                    16 0 0 0 22.46-2.81l19.64-25.27a16 16 0 0 0-2.82-22.45zm-183.72-142l-39.3-30.38A94.75 94.75 0 0 0 
-                                    416 256a94.76 94.76 0 0 0-121.31-92.21A47.65 47.65 0 0 1 304 192a46.64 46.64 0 0 1-1.54 
-                                    10l-73.61-56.89A142.31 142.31 0 0 1 320 112a143.92 143.92 0 0 1 144 144c0 21.63-5.29 41.79-13.9 60.11z">
-                                </path>
-                                </svg>
-                            </div>
-                            </div>
-                            @error('password')
-                                <div class="tw-text-sm tw-text-pink-700 tw-mt-1 tw-font-ubuntu">{{ $message }}</div>
-                            @enderror
-                        </li>
-                        <li>
-                            <select name="role" class="tw-mb-5 dropdown-toggle login-input tw-text-gray-400">
-                            <option selected>Bagian</option>
-                            <option value="1">Tata Usaha</option>
-                            <option value="2">Kesiswaan</option>
-                            <option value="3">Kurikulum</option>
-                            <option value="4">Wali Kelas</option>
-                            </select>
-                        </li>
-                        <li>
-                            <button type="button" id="sign-in-button" onclick="phoneSendAuth();" class="tw-font-ubuntu tw-bg-[#90C2C2] tw-w-full tw-py-4 tw-text-sm tw-font-medium tw-text-white hover:tw-bg-[#5B9C9C]">Daftar</button>
-                        </li>
-                        <li class="tw-text-center tw-mb-10">
-                            <a href="/login" class="tw-font-ubuntu tw-text-sims-400 tw-underline tw-text-sm">Sudah punya akun?</a>
-                        </li>
-                    </ul>
-            </section>
-                        <!-- image -->
-                        <img class="lg:tw-w-1/2 sm:tw-mb-4 sm:tw-w-full tw-items-center md:tw-m-auto" src="{{ URL::asset('assets/img/sims-login.svg') }}" alt="" srcset="">
-        </div>
-    </div>
+                <ul class="tw-flex tw-flex-col tw-gap-5">
+                    <li>
+                        <input type="text" name="nama" @error('nama') is-invalid @enderror placeholder="Nama" class="login-input">
+                        @error('nama')
+                            <div class="tw-text-sm tw-text-pink-700 tw-mt-1 tw-font-ubuntu">{{ $message }}</div>
+                        @enderror
+                    </li>
+                    <li>
+                        <input type="text" name="nip" @error('nip') is-invalid @enderror placeholder="NIP" maxlength="18" class="login-input" required>
+                        @error('nip')
+                            <div class="tw-text-sm tw-text-pink-700 tw-mt-1 tw-font-ubuntu">{{ $message }}</div>
+                        @enderror
+                    </li>
+                    <li>
+                        <input type="email" name="email" @error('email') is-invalid @enderror placeholder="Email" class="login-input">
+                        @error('email')
+                            <div class="tw-text-sm tw-text-pink-700 tw-mt-1 tw-font-ubuntu">{{ $message }}</div>
+                        @enderror
+                    </li>
+                    <li x-data="{ show: true }" class="tw-flex">
+                        <div class="tw-relative tw-w-full">
+                        <input name="password" id="password" @error('password') is-invalid @enderror placeholder="Kata Sandi" :type="show ? 'password' : 'text'" class="login-input" required>
+                        <div class="tw-absolute tw-inset-y-0 tw-right-0 tw-pr-3 tw-flex tw-items-center tw-text-sm tw-leading-5">
 
-        <div class="w-full max-w-sm" style="display: none;" id="verification_input">
-            <section class="tw-bg-white tw-font-pop tw-rounded-[50px] tw-flex tw-flex-col tw-mx-auto tw-my-28 tw-px-20 tw-py-11 tw-top-1/2 tw-w-3/5 tw-h-3/5 tw-border tw-border-slate-200 tw-shadow-xl tw-text-center">
-                <img class="tw-w-1/2 tw-mx-auto" src="{{ URL::asset('assets/img/verif-code.svg') }}" alt="Registration Success">
-                <div class="tw-text-3xl tw-font-bold tw-text-sims-400 tw-mt-5">Masukkan Kode Verifikasi</div>
-                <div class="tw-text-sm tw-text-basic-300 tw-font-medium tw-mt-3">Mohon cek SMS anda. Kami sudah mengirimkan kode verifikasi</div>
-                    <div class="tw-w-1/2 tw-mx-auto tw-mt-5">
-                        <input type="text" id="verificationCode" placeholder="Kode Verifikasi" class="tw-appearance-none login-input tw-mx-auto"
+                            <svg class="tw-h-5 tw-text-gray-500" fill="none" @click="show = !show"
+                            :class="{'tw-hidden': !show, 'tw-block':show }" xmlns="http://www.w3.org/2000/svg"
+                            viewbox="0 0 576 512">
+                            <path fill="currentColor"
+                                d="M572.52 241.4C518.29 135.59 410.93 64 288 64S57.68 135.64 3.48 241.41a32.35 
+                                32.35 0 0 0 0 29.19C57.71 376.41 165.07 448 288 448s230.32-71.64 284.52-177.41a32.35 
+                                32.35 0 0 0 0-29.19zM288 400a144 144 0 1 1 144-144 143.93 143.93 0 0 1-144 144zm0-240a95.31 
+                                95.31 0 0 0-25.31 3.79 47.85 47.85 0 0 1-66.9 66.9A95.78 95.78 0 1 0 288 160z">
+                            </path>
+                            </svg>
+
+                            <svg class="tw-h-5 tw-text-gray-500" fill="none" @click="show = !show"
+                            :class="{'tw-block': !show, 'tw-hidden':show }" xmlns="http://www.w3.org/2000/svg"
+                            viewbox="0 0 640 512">
+                            <path fill="currentColor"
+                                d="M320 400c-75.85 0-137.25-58.71-142.9-133.11L72.2 185.82c-13.79 17.3-26.48 
+                                35.59-36.72 55.59a32.35 32.35 0 0 0 0 29.19C89.71 376.41 197.07 448 320 448c26.91 
+                                0 52.87-4 77.89-10.46L346 397.39a144.13 144.13 0 0 1-26 2.61zm313.82 58.1l-110.55-85.44a331.25 
+                                331.25 0 0 0 81.25-102.07 32.35 32.35 0 0 0 0-29.19C550.29 135.59 442.93 64 320 64a308.15 308.15 
+                                0 0 0-147.32 37.7L45.46 3.37A16 16 0 0 0 23 6.18L3.37 31.45A16 16 0 0 0 6.18 53.9l588.36 454.73a16 
+                                16 0 0 0 22.46-2.81l19.64-25.27a16 16 0 0 0-2.82-22.45zm-183.72-142l-39.3-30.38A94.75 94.75 0 0 0 
+                                416 256a94.76 94.76 0 0 0-121.31-92.21A47.65 47.65 0 0 1 304 192a46.64 46.64 0 0 1-1.54 
+                                10l-73.61-56.89A142.31 142.31 0 0 1 320 112a143.92 143.92 0 0 1 144 144c0 21.63-5.29 41.79-13.9 60.11z">
+                            </path>
+                            </svg>
                         </div>
-                    <button type="submit" onclick="verify();" class="tw-font-ubuntu tw-bg-[#90C2C2] tw-py-3 text-md tw-mx-auto tw-font-medium tw-text-white tw-mt-6 hover:tw-bg-[#5B9C9C] tw-w-2/4">
-                        Verifikasi Akun
-                    </button>
-                    <button type="button" onclick="tryAgain();" class="tw-font-ubuntu tw-bg-[#90C2C2] tw-py-3 text-md tw-mx-auto tw-font-medium tw-text-white tw-mt-6 hover:tw-bg-[#5B9C9C] tw-w-2/4">
-                        Kirim Ulang
-                    </button>
-            </section> <!-- card -->
+                        </div>
+                        @error('password')
+                            <div class="tw-text-sm tw-text-pink-700 tw-mt-1 tw-font-ubuntu">{{ $message }}</div>
+                        @enderror
+                    </li>
+                    <li>
+                        <select name="role" class="tw-mb-5 dropdown-toggle login-input tw-text-gray-400">
+                        <option selected>Bagian</option>
+                        <option value="1">Tata Usaha</option>
+                        <option value="2">Kesiswaan</option>
+                        <option value="3">Kurikulum</option>
+                        <option value="4">Wali Kelas</option>
+                        </select>
+                    </li>
+                    <li>
+                        <button type="submit" class="tw-font-ubuntu tw-bg-[#90C2C2] tw-w-full tw-py-4 tw-text-sm tw-font-medium tw-text-white hover:tw-bg-[#5B9C9C]">Daftar</button>
+                    </li>
+                    <li class="tw-text-center tw-mb-10">
+                        <a href="/login" class="tw-font-ubuntu tw-text-sims-400 tw-underline tw-text-sm">Sudah punya akun?</a>
+                    </li>
+                </ul>
+            </form>
+            </section>
+
+            <!-- image -->
+            <img class="lg:tw-w-1/2 sm:tw-mb-4 sm:tw-w-full tw-items-center md:tw-m-auto" src="{{ URL::asset('assets/img/sims-login.svg') }}" alt="" srcset="">
         </div>
-    </form>
-</div> <!-- container -->
-
-{{-- alpine js --}}
-<script defer src="https://unpkg.com/@alpinejs/intersect@3.10.3/dist/cdn.min.js"></script>
-<script defer src="https://unpkg.com/alpinejs@3.10.3/dist/cdn.min.js"></script>
-
-{{-- firebase --}}
-<script src='https://cdn.firebase.com/js/client/2.2.1/firebase.js'></script>
-<script src='https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js'></script>
-
-  <script type="module">
-
-    import {
-        initializeApp
-    } from "https://www.gstatic.com/firebasejs/9.8.3/firebase-app.js";
-        
-
-    import {
-        getAuth,
-        RecaptchaVerifier,
-        signInWithPhoneNumber
-    } from 'https://www.gstatic.com/firebasejs/9.8.3/firebase-auth.js';
-    
-
-    const firebaseConfig = {
-        apiKey: "AIzaSyAQSXjcmmy5zmR495fJlZnk8XeyQfOm6Go",
-        authDomain: "127.0.0.1",
-        projectId: "sims-otp-authentication",
-        storageBucket: "sims-otp-authentication.appspot.com",
-        messagingSenderId: "640288268503",
-        appId: "1:640288268503:web:8c49350f26acf3ad019092",
-        measurementId: "G-GSZ200NT2R"
-    };
-    
-
-    const app = initializeApp(firebaseConfig);
-
-    const auth = getAuth(app);
-
-    // rechaptcha
-    window.recaptchaVerifier = new RecaptchaVerifier('sign-in-button', {
-    'size': 'invisible',
-    'callback': (response) => {
-        onSignInSubmit();
-    }
-    }, auth);
-
-
-    // send OTP
-    window.phoneSendAuth = function() {
-        var phoneNumber = $("#number").val();
-        const appVerifier = window.recaptchaVerifier;
-        const auth = getAuth();
-        signInWithPhoneNumber(auth, phoneNumber, appVerifier)
-            .then((confirmationResult) => {
-                window.confirmationResult = confirmationResult;
-                $("#phone_input").hide();
-                $("#verification_input").show();
-            }).catch((error) => {
-                alert(error.message)
-            });
-    }
-
-
-    // verify OTP
-    window.verify = function() {
-        const code = $("#verificationCode").val();
-
-        confirmationResult.confirm(code).then((result) => {
-            const user = result.user;
-
-        }).catch((error) => {
-            alert(error.message)
-        });
-    }
-
-    // try again
-    function tryAgain() {
-        $("#verification_input").hide();
-        $("#phone_input").show();
-        $("#verificationCode").val("");
-    }
-
-</script>
+    </div> <!-- container -->
+    {{-- alpine js --}}
+    <script defer src="https://unpkg.com/@alpinejs/intersect@3.10.3/dist/cdn.min.js"></script>
+    <script defer src="https://unpkg.com/alpinejs@3.10.3/dist/cdn.min.js"></script>
 </body>
 </html>
