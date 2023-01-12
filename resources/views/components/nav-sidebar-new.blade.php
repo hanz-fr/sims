9a41077ef9adb1fcd53b9aea908ec3884f3cb132
recta-rrae <raihanf787@gmail.com>
2023-01-12 09:31:22 +0700

    removed mobile navbar

--- resources/views/components/nav-sidebar-new.blade.php
+++ resources/views/components/nav-sidebar-new.blade.php
@@ -6,31 +6,6 @@
                 class="tw-fixed tw-inset-0 tw-z-10 tw-bg-black tw-bg-opacity-50 lg:tw-hidden"></div>
             <div x-show="isSidebarOpen" class="tw-fixed tw-inset-y-0 tw-z-10 tw-w-16 tw-bg-white"></div>
 
-            <!-- Mobile bottom bar -->
-            <nav aria-label="Options"
-                class="tw-fixed tw-inset-x-0 tw-bottom-0 tw-flex tw-flex-row-reverse tw-items-center tw-justify-between tw-px-4 tw-py-2 tw-bg-white tw-border-t tw-border-indigo-100 sm:tw-hidden tw-shadow-t tw-rounded-t-3xl">
-                <!-- Menu button -->
-                <button
-                    @click="(isSidebarOpen && currentSidebarTab == 'linksTab') ? isSidebarOpen = false : isSidebarOpen = true; currentSidebarTab = 'linksTab'"
-                    class="tw-p-2 tw-transition-colors tw-rounded-lg tw-shadow-md hover:tw-bg-indigo-800 hover:tw-text-white focus:tw-outline-none focus:tw-ring focus:tw-ring-indigo-600 focus:tw-ring-offset-white focus:tw-ring-offset-2"
-                    :class="(isSidebarOpen && currentSidebarTab == 'linksTab') ? 'text-white bg-indigo-600' :
-                    'text-gray-500 bg-white'">
-                    <span class="sr-only">Toggle sidebar</span>
-                    <svg aria-hidden="true" class="tw-w-6 tw-h-6" xmlns="http://www.w3.org/2000/svg" fill="none"
-                        viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="M4 6h16M4 12h16M4 18h7" />
-                    </svg>
-                </button>
-
-                <!-- Logo -->
-                <a href="#">
-                    <img class="tw-w-10 tw-h-auto"
-                        src="https://raw.githubusercontent.com/kamona-ui/dashboard-alpine/main/public/assets/images/logo.png"
-                        alt="K-UI" />
-                </a>
-            </nav>
-
             <!-- Left mini bar -->
             <nav aria-label="Options"
                 class="tw-z-20 tw-flex-col tw-items-center tw-flex-shrink-0 tw-hidden tw-w-24 tw-py-4 tw-bg-white tw-border-r-2 tw-border-indigo-100 sm:tw-flex">
@@ -120,7 +95,7 @@
                               :aria-expanded="open"
                               :aria-controls="$id('dropdown-button')"
                               type="button" x-tooltip.placement.right.delay.500-100="tooltip" type="button"
-                              class="{{ ($active === "data-induk") ? 'tw-bg-sims-400 tw-text-white' : 'tw-text-sims-400' }} tw-p-5 tw-transition-colors tw-rounded-xl hover:tw-bg-sims-400 hover:tw-text-white focus:tw-outline-none">
+                              class="tw-text-sims-new-500 tw-p-5 tw-transition-colors tw-rounded-xl hover:tw-bg-sims-new-500 hover:tw-text-white focus:tw-outline-none">
                               <span class="sr-only">Toggle sidebar</span>
                               <i class="fa-solid fa-circle-user tw-text-3xl"></i>
                           </button>
@@ -140,78 +115,7 @@
                       </div>
                 </div>
             </nav>
-
-            <div x-transition:enter="tw-transform tw-transition-transform tw-duration-300"
-                x-transition:enter-start="-tw-translate-x-full" x-transition:enter-end="tw-translate-x-0"
-                x-transition:leave="tw-transform tw-transition-transform tw-duration-300"
-                x-transition:leave-start="tw-translate-x-0" x-transition:leave-end="-tw-translate-x-full"
-                x-show="isSidebarOpen"
-                class="tw-fixed tw-inset-y-0 tw-left-0 tw-z-10 tw-flex-shrink-0 tw-w-64 tw-bg-white tw-border-r-2 tw-border-indigo-100 tw-shadow-lg sm:tw-left-16 tw-rounded-tr-3xl tw-rounded-br-3xl sm:tw-w-72 lg:tw-static lg:tw-w-64">
-                <nav x-show="currentSidebarTab == 'linksTab'" aria-label="Main" class="flex flex-col h-full">
-                    <!-- Logo -->
-                    <div class="tw-flex tw-items-center tw-justify-center tw-flex-shrink-0 tw-py-10">
-                        <a href="#">
-                            <img class="tw-w-24 tw-h-auto"
-                                src="https://raw.githubusercontent.com/kamona-ui/dashboard-alpine/main/public/assets/images/logo.png"
-                                alt="K-UI" />
-                        </a>
-                    </div>
-
-                    <!-- Links -->
-                    <div class="tw-flex-1 tw-px-4 tw-space-y-2 tw-overflow-hidden hover:tw-overflow-auto">
-                        <a href="#"
-                            class="tw-flex tw-items-center tw-w-full tw-space-x-2 tw-text-white tw-bg-indigo-600 rounded-lg">
-                            <span aria-hidden="true" class="tw-p-2 tw-bg-indigo-700 tw-rounded-lg">
-                                <svg class="tw-w-6 tw-h-6" xmlns="http://www.w3.org/2000/svg" fill="none"
-                                    viewBox="0 0 24 24" stroke="currentColor">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                        d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
-                                </svg>
-                            </span>
-                            <span>Home</span>
-                        </a>
-                        <a href="#"
-                            class="tw-flex tw-items-center tw-space-x-2 tw-text-indigo-600 tw-transition-colors tw-rounded-lg tw-group hover:tw-bg-indigo-600 hover:tw-text-white">
-                            <span aria-hidden="true"
-                                class="tw-p-2 tw-transition-colors tw-rounded-lg group-hover:tw-bg-indigo-700 group-hover:tw-text-white">
-                                <svg class="tw-w-6 tw-h-6" xmlns="http://www.w3.org/2000/svg" fill="none"
-                                    viewBox="0 0 24 24" stroke="currentColor">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                        d="M7 21h10a2 2 0 002-2V9.414a1 1 0 00-.293-.707l-5.414-5.414A1 1 0 0012.586 3H7a2 2 0 00-2 2v14a2 2 0 002 2z" />
-                                </svg>
-                            </span>
-                            <span>Pages</span>
-                        </a>
-                    </div>
-
-                    <div class="flex-shrink-0 p-4 mt-10">
-                        <div class="hidden p-2 space-y-6 bg-gray-100 rounded-lg md:block">
-                            <img aria-hidden="true" class="-mt-10"
-                                src="https://raw.githubusercontent.com/kamona-ui/dashboard-alpine/52b4b4abb92ef251f6610be416038b48209d7a81/public/assets/images/undraw_web_developer_p3e5.svg" />
-                            <p class="text-sm text-indigo-600">
-                                Use our <span class="text-base text-indigo-700er">Premium</span> features now! <br />
-                            </p>
-                            <button
-                                class="w-full px-4 py-2 text-center text-white transition-colors bg-indigo-600 rounded-lg hover:bg-indigo-700 focus:outline-none focus:ring focus:ring-indigo-600 focus:ring-offset-2 focus:ring-offset-gray-100">
-                                Upgrade to pro
-                            </button>
-                        </div>
-
-                        <button
-                            class="w-full px-4 py-2 text-center text-white transition-colors bg-indigo-600 rounded-lg md:hidden hover:bg-indigo-700 focus:outline-none focus:ring focus:ring-indigo-600 focus:ring-offset-2 focus:ring-offset-gray-100">
-                            Upgrade to pro
-                        </button>
-                    </div>
-                </nav>
-
-                <section x-show="currentSidebarTab == 'messagesTab'" class="px-4 py-6">
-                    <h2 class="text-xl">Messages</h2>
-                </section>
-
-                <section x-show="currentSidebarTab == 'notificationsTab'" class="px-4 py-6">
-                    <h2 class="text-xl">Notifications</h2>
-                </section>
-            </div>
+            
         </aside>
         <div class="flex flex-col flex-1 tw-bg-white">
 
--- resources/views/induk/show-all.blade.php
+++ resources/views/induk/show-all.blade.php
@@ -296,7 +296,7 @@
                     @else
 
                     <!-- This is an example component -->
-                    <div class="tw-mx-5">
+                    {{-- <div class="tw-mx-5">
                         <select id="show-tahun-ajaran" class="tw-border tw-border-none tw-text-sims-new-500 hover:tw-text-white tw-font-pop tw-text-sm tw-font-medium tw-rounded-lg tw-h-10 tw-pl-5 tw-pr-10 tw-bg-white hover:tw-bg-sims-new-500 hover:tw-border-sims-new-500 focus:tw-outline-none tw-appearance-none focus:tw-ring-0 tw-shadow-md tw-transition-all tw-ease-in-out" style="cursor: pointer;">
                         
                           @if(!empty($_GET['thn_ajaran'])) 
@@ -325,7 +325,7 @@
                                 @endwhile
                           @endfor
                         </select>
-                    </div>
+                    </div> --}}
 
 
                     @endif
