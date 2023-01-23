aeab0ca84c04fa8ef594cfacc64402f46b7b073e
dirarmdn <dhiraramadini@gmail.com>
2023-01-23 18:42:50 +0700

    help centre update

--- resources/views/userguide/index.blade.php
+++ resources/views/userguide/index.blade.php
@@ -13,138 +13,78 @@
     </div>
     
     <!-- spacing !-->
-    <div class="tw-mt-24"></div> 
+    <div class="tw-mt-20"></div> 
     <!-------------->
 
     <div class="tw-mx-20">
 
-        <div class="hc-title-lg"> 
-             Butuh Bantuan?
-        </div>
-        <div class="hc-text-base">
-            Jangan khawatir! Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
-            In dicta dolore expedita ex totam cupiditate asperiores, 
-            aperiam magni delectus excepturi quis distinctio laboriosam 
-            ea quidem, aliquam eius? Eveniet in ab fuga placeat, 
-            excepturi fugit sit eius nam illo accusantium iusto, 
-            harum numquam commodi possimus rerum dolores enim 
-            debitis consectetur ipsam sequi laudantium tempora? 
-            Suscipit incidunt provident recusandae fugit in esse 
-            doloremque neque porro minima! Maxime aut dolor 
-            suscipit hic deleniti, dolores excepturi autem. 
-            Enim, distinctio aspernatur labore, 
-            similique voluptates facilis porro tenetur 
-            esse at accusantium laborum architecto odio 
-            voluptatibus earum rerum perspiciatis, 
-            quas cum aperiam reprehenderit cumque. 
-            Enim, nisi quo!
-        </div>
-        <br>
-        <div class="hc-text-base">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Aliquid itaque est quia expedita explicabo quod deserunt,
-            ad sunt molestiae sequi at ipsum,
-            voluptatum reiciendis doloribus ratione,
-            natus neque a porro iure dicta. Animi,
-            earum sunt laborum nam quis inventore iusto incidunt
-            assumenda repellendus laudantium ipsa dolore sequi exercitationem
-            voluptatibus dignissimos facilis! Libero sint delectus
-            repudiandae unde nisi ratione officia aliquid illum ex aliquam.
-            Soluta illum nam ratione sint beatae laudantium molestias
-            possimus est quisquam iusto officia dicta voluptatibus,
-            ipsam natus illo facere placeat voluptatum alias reiciendis
-            molestiae labore! Voluptatum praesentium placeat quo,
-            a mollitia corporis, omnis ipsa veritatis facilis maxime
-            dolorem consequatur consectetur voluptatem tempora.
-            Cumque aspernatur dolorum magnam tenetur nobis nemo
-            corporis facere animi quos voluptates veniam,
-            voluptate autem doloremque. Tempore obcaecati
-            magnam aspernatur error natus voluptate neque
-            explicabo sequi consectetur porro vitae quas
-            dignissimos deleniti autem nulla at nostrum
-            labore, nisi consequatur? Suscipit exceptur
-            natus praesentium provident nam animi vero iure possimus,
-            totam, officiis facilis facere quis reprehenderit debitis
-            dolorum sapiente sunt quidem sequi. Animi nemo quis
-            officiis maiores aperiam fugiat asperiores suscipit
-            veniam consectetur sunt, nobis reprehenderit illum
-            voluptas labore architecto autem. Amet repudiandae
-            veniam quam adipisci repellendus illum, et, nobis natus
-            recusandae, sit assumenda laborum provident.
+
+        <div class="tw-font-satoshi tw-text-2xl hc-text-lg">
+            Butuh bantuan menggunakan fitur-fitur SIMS? Jangan Khawatir!
+            Semua yang Anda butuhkan untuk mengakses fitur-fitur kami tersedia disini. Pelajari dan temukan berbagai manfaat menggunakan layanan kami.
         </div>
 
         
+        <div class="tw-text-basic-700 tw-text-center hc-title-3xl tw-mt-20"> 
+            <span class="tw-text-sims-500">Bagaimana</span> kami dapat membantu anda?
+        </div>
+
+        <br>
+
+        <div class="tw-items-center tw-my-7 tw-text-center">
+            <form class="tw-border-none tw-font-sg tw-p-0 tw-items-center tw-mb-10">
+                <input type="text" id="cardFilter" onkeyup="cardFilters()" class="tw-rounded-lg tw-w-5/6 tw-h-10 tw-items-center tw-border-gray-200 hover:tw-border-gray-300 focus-within:tw-border-gray-300 focus:tw-ring-gray-300 hc-search-input" placeholder="cari fitur...">
+            </form>
+        </div>
+
+        <div class="hc-text-xl tw-text-center tw-font-satoshi tw-mb-20">
+            Atau pilih di bawah ini untuk segera menemukan informasi mengenai fitur kami
+        </div>
 
         <!-- card !--> 
-        <div class="tw-flex tw-flex-wrap-reverse tw-justify-center tw-gap-x-5 tw-gap-y-10 tw-my-24">
+        <div id="cardFeature" class="tw-flex tw-flex-wrap tw-justify-center tw-gap-x-7 tw-gap-y-10 tw-mb-24">
 
             <a href="#" class="hc-card">
-                <i class="fa-sharp fa-regular fa-book-open tw-text-3xl tw-text-sims-400 tw-mb-2"></i>
-                <div class="hc-title-lg">Data Induk</div>
-                <div class="hc-text-xs">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, consequatur!</div>
+                <i class="fa-regular fa-graduation-cap tw-text-4xl tw-text-sims-400 tw-mb-2"></i>
+                <div class="hc-title-xl">Alumni</div>
+                <div class="hc-text-base">Penyimpanan data induk dari alumni sekolah selama 5 tahun terakhir!</div>
             </a>
 
             <a href="#" class="hc-card">
-                <i class="fa-regular fa-users tw-text-3xl tw-text-sims-400 tw-mb-2"></i>
-                <div class="hc-title-lg">Mutasi</div>
-                <div class="hc-text-xs">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, consequatur!</div>
+                <i class="fa-sharp fa-regular fa-book-open tw-text-4xl tw-text-sims-400 tw-mb-2"></i>
+                <div class="hc-title-xl">Data Induk</div>
+                <div class="hc-text-base">Pengelolaan dan penyimpanan data induk secara digital dan real-time</div>
             </a>
 
             <a href="#" class="hc-card">
-                <i class="fa-regular fa-book tw-text-3xl tw-text-sims-400 tw-mb-2"></i>
-                <div class="hc-title-lg">Rekap Nilai</div>
-                <div class="hc-text-xs">Lorem ipsum, dolor sit amet consectetur adipisicing elit</div>
+                <i class="fa-regular fa-users tw-text-4xl tw-text-sims-400 tw-mb-2"></i>
+                <div class="hc-title-xl">Mutasi</div>
+                <div class="hc-text-base">Mengelola data mutasi masuk dan keluar, solusi tepat untuk mengelola data mutasi</div>
             </a>
 
             <a href="#" class="hc-card">
-                <i class="fa-regular fa-user tw-text-3xl tw-text-sims-400 tw-mb-2"></i>
-                <div class="hc-title-lg">Akun</div>
-                <div class="hc-text-xs">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, consequatur!</div>
+                <i class="fa-regular fa-book tw-text-4xl tw-text-sims-400 tw-mb-2"></i>
+                <div class="hc-title-xl">Rekap Nilai</div>
+                <div class="hc-text-base">Penyimpanan rekap nilai setiap siswa, mempermudah penginputan rekap nilai</div>
+            </a>
+
+            <a href="#" class="hc-card">
+                <i class="fa-regular fa-user tw-text-4xl tw-text-sims-400 tw-mb-2"></i>
+                <div class="hc-title-xl">Akun</div>
+                <div class="hc-text-base">Pelajari tentang akun dan keamaan pengguna di SIMS</div>
             </a>
 
             <a href="/help/general" class="hc-card">
-                <i class="fa-solid fa-question tw-text-3xl tw-text-sims-400 tw-mb-2"></i>
-                <div class="hc-title-lg">SIMS</div>
-                <div class="hc-text-xs">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut, nemo?</div>
+                <i class="fa-solid fa-question tw-text-4xl tw-text-sims-400 tw-mb-2"></i>
+                <div class="hc-title-xl">SIMS</div>
+                <div class="hc-text-base">Pelajari hal-hal mendasar tentang SIMS</div>
             </a>
 
         </div>
-
-        <div class="hc-title-lg"> 
-            Lorem Ipsum dolor sit Amet
-        </div>
-        <div class="hc-text-base">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Aliquid itaque est quia expedita explicabo quod deserunt,
-            ad sunt molestiae sequi at ipsum,
-            voluptatum reiciendis doloribus ratione,
-            natus neque a porro iure dicta. Animi,
-            earum sunt laborum nam quis inventore iusto incidunt
-            assumenda repellendus laudantium ipsa dolore sequi exercitationem
-            voluptatibus dignissimos facilis! Libero sint delectus
-            repudiandae unde nisi ratione officia aliquid illum ex aliquam.
-            Soluta illum nam ratione sint beatae laudantium molestias
-            possimus est quisquam iusto officia dicta voluptatibus,
-            ipsam natus illo facere placeat voluptatum alias reiciendis
-            molestiae labore! Voluptatum praesentium placeat quo,
-            a mollitia corporis, omnis ipsa veritatis facilis maxime
-            dolorem consequatur consectetur voluptatem tempora.
-            Cumque aspernatur dolorum magnam tenetur nobis nemo
-            corporis facere animi quos voluptates veniam,
-            voluptate autem doloremque. Tempore obcaecati
-            magnam aspernatur error natus voluptate neque
-            explicabo sequi consectetur porro vitae quas
-            dignissimos deleniti autem nulla at nostrum
-            labore, nisi consequatur? Suscipit exceptur
-            natus praesentium provident nam animi vero iure possimus,
-            totam, officiis facilis facere quis reprehenderit debitis
-            dolorum sapiente sunt quidem sequi. Animi nemo quis
-            officiis maiores aperiam fugiat asperiores suscipit
-            veniam consectetur sunt, nobis reprehenderit illum
-            voluptas labore architecto autem. Amet repudiandae
-            veniam quam adipisci repellendus illum, et, nobis natus
-            recusandae, sit assumenda laborum provident.
-        </div>
+{{-- 
+        <div class="tw-text-basic-700 tw-text-center hc-title-3xl tw-mt-10"> 
+            <span class="tw-text-sims-500">FAQ</span> SIMS
+        </div> --}}
 
 
         <!-- spacing !-->
@@ -154,4 +94,24 @@
 
 </div>
 
+<script>
+function cardFilters() {
+    var input, filter, cards, cardContainer, title, i;
+
+    input = document.getElementById("cardFilter");
+    filter = input.value.toUpperCase();
+    cardContainer = document.getElementById("cardFeature");
+    cards = cardContainer.getElementsByClassName("hc-card");
+
+    for (i = 0; i < cards.length; i++) {
+        title = cards[i].querySelector(".hc-card div.hc-title-xl");
+
+        if (title.innerText.toUpperCase().indexOf(filter) > -1) {
+            cards[i].style.display = "";
+        } else {
+            cards[i].style.display = "none";
+        }
+    }
+}
+</script>
 @endsection
